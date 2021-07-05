0d1cd5ecab43fe808bfe6d58ae542ccba58486eb
rgbui <760489033@qq.com>
2021-07-05 19:40:47 +0800

    添加login

---
+++ src/user/login.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Login extends React.Component {
+    render() {
+        return <div className='sy-login'>
+            <div className='sy-login-head'></div>
+        </div>
+    }
+}
