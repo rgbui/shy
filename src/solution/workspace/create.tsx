ba87cf6fe86f507d5ce02ab1292a75bc31f8bd66
kanhai <760489033@qq.com>
2021-07-14 00:20:13 +0800

    实现创建workspace的功能

--- src/solution/workspace/create.tsx
+++ src/solution/workspace/create.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { generatePath } from "react-router";
 import { SyHistory } from "../../history";
-import { masterSock } from "../../service/sock";
 import { workspaceService } from "../service";
 
 export class WorkspaceCreateView extends React.Component {
@@ -14,22 +14,20 @@ export class WorkspaceCreateView extends React.Component {
         button.disabled = true;
         try {
             var rr = await workspaceService.createWorkspace({ text: this.text })
-            if (rr.ok) return SyHistory.push('/ws/:id', { id: rr.data.id });
+            if (rr.ok) return SyHistory.push(generatePath('/ws/:id', { id: rr.data.id }));
             else this.failTip = rr.warn;
             this.forceUpdate();
         }
         catch (ex) {
 
         }
-        finally {
-            button.disabled = false;
-        }
+        button.disabled = false;
     }
     render() {
-        return <div className='shy-workspace-create'>
-            <div className='shy-workspace-create-text'><input defaultValue={this.text} onInput={e => this.setText(e)} /></div>
-            <div className='shy-worksace-create-fail-tip'>{this.failTip}</div>
-            <div className='shy-worksace-create-button'><button onClick={e => this.save(e)}>创建空间</button></div>
+        return <div className='sy-workspace-create'>
+            <div className='sy-workspace-create-text'><input className='input' defaultValue={this.text} onInput={e => this.setText(e)} /></div>
+            <div className='sy-workspace-create-fail-tip'>{this.failTip}</div>
+            <div className='sy-workspace-create-button'><button className='button' onClick={e => this.save(e)}>创建空间</button></div>
         </div>
     }
 }
