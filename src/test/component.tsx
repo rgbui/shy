524e900ea560a6914c19a8b3bf980ee65961b9bf
kanhai <760489033@qq.com>
2021-08-06 23:50:15 +0800

    测试tab组件

--- src/test/component.tsx
+++ src/test/component.tsx
@@ -4,6 +4,7 @@ import { Select } from "rich/component/select";
 import { Input } from "rich/component/input";
 import { Switch } from "rich/component/switch";
 import { Button } from "rich/component/button";
+import { Tab } from 'rich/component/tab';
 export function Component() {
     var [checked, setChecked] = React.useState(false);
     var [name, setName] = React.useState('kanhai');
@@ -19,5 +20,10 @@ export function Component() {
         <Button block>你好</Button>
         <Button ghost>你好</Button>
         <Button>你好</Button>
+        <Tab>
+            <Tab.Page item={'你好'}>1</Tab.Page>
+            <Tab.Page item={'我好'}>2</Tab.Page>
+            <Tab.Page item={'它好'}>3</Tab.Page>
+        </Tab>
     </div>
 }
