5172caa595f4583c2f6e9288cd78b2d8a9180bbe
kanhai <760489033@qq.com>
2021-08-13 23:04:37 +0800

    实现头像组件

--- src/components/face.tsx
+++ src/components/face.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-export class Avatar extends React.Component {
+import { IconArguments } from 'rich/extensions/icon/declare';
+import "./style.less";
+export class Avatar extends React.Component<{
+    icon: IconArguments,
+    text: string,
+    size?: number,
+    circle?: boolean
+}> {
     render() {
+        var size = this.props.size ? this.props.size : 20;
         return <div className='shy-avatar'>
-            <img />
+            {this.props.icon && <img style={{ width: size, height: size }} src={this.props.icon.url} />}
+            {!this.props.icon && <span style={{ width: size, height: size }} className='shy-avatar-name'>{this.props.text.slice(0, 1)}</span>}
         </div>
     }
 }
