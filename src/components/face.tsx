07285a8e5dc2bcc1591f174a78acec58a15fa6c9
kanhai <760489033@qq.com>
2021-08-19 23:28:14 +0800

    新增头像组件支持点击事件

--- src/components/face.tsx
+++ src/components/face.tsx
@@ -5,11 +5,14 @@ export class Avatar extends React.Component<{
     icon: IconArguments,
     text: string,
     size?: number,
-    circle?: boolean
+    circle?: boolean,
+    onClick?: (event: React.MouseEvent) => void,
 }> {
     render() {
         var size = this.props.size ? this.props.size : 20;
-        return <div className='shy-avatar'>
+        return <div className='shy-avatar' onClick={e => {
+            if (typeof this.props.onClick == 'function') this.props.onClick(e);
+        }}>
             {this.props.icon && <img style={{ width: size, height: size }} src={this.props.icon.url} />}
             {!this.props.icon && <span style={{ width: size, height: size, fontSize: size * 0.8, lineHeight: size + 'px' }} className='shy-avatar-name'>{this.props.text.slice(0, 1)}</span>}
         </div>
