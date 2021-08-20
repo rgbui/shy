bff084d5ba1ceecd0fc329f48020802bae879a9e
kanhai <760489033@qq.com>
2021-08-20 22:26:36 +0800

    实现通过用户的userid自动显示用户的头像

--- src/components/face.tsx
+++ src/components/face.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { IconArguments } from 'rich/extensions/icon/declare';
+import { masterSock } from '../service/sock';
 import "./style.less";
 export class Avatar extends React.Component<{
-    icon: IconArguments,
-    text: string,
+    icon?: IconArguments,
+    text?: string,
     size?: number,
     circle?: boolean,
     onClick?: (event: React.MouseEvent) => void,
+    userid?: string
 }> {
+    private userFaceUrl: string;
+    async componentDidMount() {
+        if (!this.userFaceUrl) {
+            var base_url = await masterSock.getBaseUrl();
+            this.userFaceUrl = masterSock.resolve(base_url, `/user/avatar/${this.props.userid}`)
+        }
+    }
     render() {
         var size = this.props.size ? this.props.size : 20;
+        var renderIcon = () => {
+            if (this.props.userid) {
+                return this.userFaceUrl && <img style={{ width: size, height: size }} src={this.userFaceUrl} />
+            }
+            else {
+                return <>
+                    {this.props.icon && <img style={{ width: size, height: size }} src={this.props.icon.url} />}
+                    {!this.props.icon && <span style={{ width: size, height: size, fontSize: size * 0.8, lineHeight: size + 'px' }}
+                        className='shy-avatar-name'>{this.props.text.slice(0, 1)}</span>}
+                </>
+            }
+        }
         return <div className='shy-avatar' onClick={e => {
             if (typeof this.props.onClick == 'function') this.props.onClick(e);
         }}>
-            {this.props.icon && <img style={{ width: size, height: size }} src={this.props.icon.url} />}
-            {!this.props.icon && <span style={{ width: size, height: size, fontSize: size * 0.8, lineHeight: size + 'px' }} className='shy-avatar-name'>{this.props.text.slice(0, 1)}</span>}
+            {renderIcon()}
         </div>
     }
 }
