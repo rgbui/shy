8c91a277bc94fd84612c74b432b063921a737e28
kanhai <760489033@qq.com>
2021-08-21 11:25:42 +0800

    实现空间属性的编辑

--- src/workspace/settings/settings.tsx
+++ src/workspace/settings/settings.tsx
@@ -2,23 +2,81 @@ import React from 'react';
 import { Button } from 'rich/component/button';
 import { Row, Col, Divider, Space } from 'rich/component/grid';
 import { Input } from 'rich/component/input';
+import { Workspace } from '..';
+import { OpenFileDialoug } from 'rich/component/file';
 import { Avatar } from '../../components/face';
 import { surface } from '../../surface';
-export class WorkspaceSettingsView extends React.Component {
+import { Directive } from 'rich/util/bus/directive';
+import { messageChannel } from 'rich/util/bus/event.bus';
+import "./style.less";
+export class WorkspaceSettingsView extends React.Component<{ close: () => void }> {
+    async onUploadFace() {
+        var file = await OpenFileDialoug({ exts: ['image/*'] });
+        if (file) {
+            var r = await messageChannel.fireAsync(Directive.UploadFile, file, (event) => { });
+            if (r.ok) {
+                if (r.data.url) {
+                    surface.workspace.onUpdateInfo({ icon: { name: 'image', url: r.data.url } })
+                }
+            }
+        }
+    }
+    data: Partial<Workspace> = {
+        text: '',
+        slogan: '',
+        customizeSecondDomain: '',
+    }
+    setData(data: WorkspaceSettingsView['data']) {
+        if (typeof data.text != 'undefined' && data.text) {
+            this.error.text = '';
+            if (data.text.length > 30) {
+                this.error.text = '呢称过长';
+                return;
+            }
+        }
+        if (typeof data.slogan != 'undefined' && data.slogan) {
+            this.error.slogan = '';
+            if (data.slogan.length > 140) {
+                this.error.slogan = '介绍过长,不能超过140个字符';
+                return;
+            }
+        }
+        if (typeof data.customizeSecondDomain != 'undefined' && data.customizeSecondDomain) {
+            this.error.customizeDomain = '';
+            if (data.customizeDomain.length > 30) {
+                this.error.customizeDomain = '二级域名不合法';
+                return;
+            }
+        }
+        Object.assign(this.data, data);
+    }
+    error: Partial<Workspace> = {
+        text: '',
+        slogan: ''
+    }
+    async onSave() {
+        if (this.error.text || this.error.slogan) return;
+        await surface.workspace.onUpdateInfo({ text: this.data.text, customizeSecondDomain: this.data.customizeSecondDomain, slogan: this.data.slogan });
+        this.onClose();
+    }
+    onClose() {
+        if (typeof this.props.close == 'function')
+            this.props.close()
+    }
     render() {
         return <div className='shy-settings-content-form'>
             <div className='shy-settings-content-form-main'>
                 <Row><h2>空间设置</h2></Row>
                 <Divider></Divider>
                 <Row>
-                    <Col span={12} align='start'><Avatar size={70} text={surface.workspace.text} icon={surface.workspace.icon}></Avatar></Col>
-                    <Col span={12} align='end'><Button>上传图片</Button></Col>
+                    <Col span={12} align='start'><Avatar onClick={() => this.onUploadFace()} size={70} text={surface.workspace.text} icon={surface.workspace.icon}></Avatar></Col>
+                    <Col span={12} align='end'><Button onClick={() => this.onUploadFace()}>上传图片</Button></Col>
                 </Row>
                 <Divider></Divider>
                 <Row>
                     <h5>工作空间名称</h5>
                     <label>点击输入框可修改名称</label>
-                    <Input placeholder={'请输入你的工作空间名称'}></Input>
+                    <Input value={surface.workspace.text} onChange={e => this.setData({ text: e })} placeholder={'请输入你的工作空间名称'}></Input>
                 </Row>
                 <Divider></Divider>
                 <Row>
@@ -26,13 +84,16 @@ export class WorkspaceSettingsView extends React.Component {
                     <label>设置你的专属工作空间域名:https://domain.shy.live</label>
                     <div className='shy-ws-settings-view-domain'>
                         <span>https://</span>
-                        <Input placeholder={'domain'} style={{ width: 180 }}></Input>
+                        <Input
+                            value={surface.workspace.customizeSecondDomain}
+                            onChange={e => this.setData({ customizeSecondDomain: e })}
+                            placeholder={'domain'} style={{ display: 'inline-block', width: 120 }}></Input>
                         <span>.shy.live</span>
                     </div>
                 </Row>
             </div>
             <div className='shy-settings-content-form-footer'>
-                <Space align='end' style={{ height: '100%' }}><Button>取消</Button><Button>保存</Button></Space>
+                <Space align='end' style={{ height: '100%' }}><Button >取消</Button><Button>保存</Button></Space>
             </div>
         </div>
     }
