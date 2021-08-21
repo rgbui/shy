05610cbef325f1696714033879909168bd852da7
kanhai <760489033@qq.com>
2021-08-21 11:25:19 +0800

    实现空间用户的管理

--- src/workspace/settings/member.tsx
+++ src/workspace/settings/member.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Row, Col } from 'rich/component/grid';
+import { Row, Col, Divider } from 'rich/component/grid';
 import { Input } from 'rich/component/input';
 import { Button } from 'rich/component/button';
 import { CopyText } from 'rich/component/copy';
@@ -8,6 +8,7 @@ import { surface } from '../../surface';
 import { workspaceService } from '../service';
 import { Avatar } from '../../components/face';
 import { Select } from 'rich/component/select';
+
 export class WorkspaceMembers extends React.Component {
     async createInvite() {
         if (!surface.workspace.inviteUrl) {
@@ -28,8 +29,9 @@ export class WorkspaceMembers extends React.Component {
             <div className='shy-ws-memeber-invitation'>
                 <h4>通用邀请链接</h4>
                 <div className='shy-remark'>任何得到此链接的人均可以加入这个空间，你也可以</div>
-                <Row><Col span={16}><Input></Input></Col><Col align='start' span={8}><Button onClick={e => this.createInvite()} ghost>{surface.workspace.inviteUrl ? '复制链接' : '创建链接'}</Button></Col></Row>
+                <Row><Col span={16}><Input></Input></Col><Col align='start' span={8}><Button style={{ marginLeft: 20 }} onClick={e => this.createInvite()} ghost>{surface.workspace.inviteUrl ? '复制链接' : '创建链接'}</Button></Col></Row>
             </div>
+            <Divider></Divider>
             <div className='shy-ws-members-list'>
                 <div className='shy-ws-member-head'>
                     <Row>
@@ -37,12 +39,13 @@ export class WorkspaceMembers extends React.Component {
                         <Col span={12} align='end'><Button onClick={e => this.onAddMember()}>添加用户</Button></Col>
                     </Row>
                 </div>
+                <Divider></Divider>
                 {surface.workspace.users.map(us => {
                     return <div className='shy-ws-member' key={us.userid}>
                         <Row>
                             <Col span={16}> <Avatar userid={us.userid}></Avatar>
                                 <span>{us.nick}</span></Col>
-                            <Col span={8} align='end'><Select style={{width:80}} value={us.role} options={[{ text: '管理员', value: '管理员' }, { text: '普通', value: '普通' }]}></Select></Col>
+                            <Col span={8} align='end'><Select style={{ width: 80 }} value={us.role} options={[{ text: '管理员', value: '管理员' }, { text: '普通', value: '普通' }]}></Select></Col>
                         </Row>
                     </div>
                 })}
