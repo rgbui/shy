f1f21fae5dfd7d05136de4a21c52fda6d06e95cb
kanhai <760489033@qq.com>
2021-10-05 21:50:41 +0800

    增加了App组件，实现i18n预加载的功能

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
+import { appLangProvider } from '../../i18n/provider';
 import { SyHistory } from './history';
 import { ViewSurface } from './surface/view';
 import { Login } from './user/login';
 import { WorkspaceCreateView } from './workspace/create';
 export function App() {
+  let [isLoad, setLoad] = React.useState(false);
+  async function load() {
+    await appLangProvider.import();
+    setLoad(true);
+  }
+  React.useEffect(() => {
+    load();
+  },[])
+  return <div className='shy-app'>
+    {!isLoad && <div className='shy-app-load'></div>}
+    {isLoad && <Router history={SyHistory}>
+      <Route path='/' exact component={ViewSurface}></Route>
+      <Route path='/login' exact component={Login}></Route>
+      <Route path='/ws/:id' exact component={ViewSurface}></Route>
+      <Route path='/page/:id' exact component={ViewSurface}></Route>
+      <Route path='/work/create' exact component={WorkspaceCreateView}></Route>
+      <Route path='/test/component' exact component={Component}></Route>
+    </Router>}
 
-  return <div className='shy-app'><Router history={SyHistory}>
-    <Route path='/' exact component={ViewSurface}></Route>
-    <Route path='/login' exact component={Login}></Route>
-    <Route path='/ws/:id' exact component={ViewSurface}></Route>
-    <Route path='/page/:id' exact component={ViewSurface}></Route>
-    <Route path='/work/create' exact component={WorkspaceCreateView}></Route>
-    <Route path='/test/component' exact component={Component}></Route>
-  </Router>
   </div>
 }
