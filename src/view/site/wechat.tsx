74b3bb792c389f9a377fb1ee3d6cca2300ae0128
kanhai <760489033@qq.com>
2021-10-17 22:25:14 +0800

    官网首页调整

--- src/view/site/wechat.tsx
+++ src/view/site/wechat.tsx
@@ -2,10 +2,13 @@ import { observer } from "mobx-react";
 import React from 'react';
 import { FooterView } from "./layout/footer";
 import { HeadView } from "./layout/head";
+import pic from "../../assert/img/pic.jpg";
 export var WeChatView = observer(function () {
     return <div className='shy-site'>
         <HeadView></HeadView>
-        <div className='shy-site-content'> </div>
+        <div className='shy-site-content'>
+            <img src={pic} style={{ width: '100%' }} />
+        </div>
         <FooterView></FooterView>
     </div>
 })
