5159a821f4f100f73a6dbcda6f070d4f65f1143a
kanhai <760489033@qq.com>
2021-10-23 23:06:29 +0800

    完全实现文档公开至互联网的功能

--- src/view/supervisor/publish/index.tsx
+++ src/view/supervisor/publish/index.tsx
@@ -5,6 +5,9 @@ import { Switch } from "rich/component/view/switch";
 import { PopoverPosition } from "rich/extensions/popover/position";
 import { PageItem } from "../../sln/item";
 import { workspaceService } from "../../../../services/workspace";
+import { PopoverSingleton } from "rich/extensions/popover/popover";
+import "./style.less";
+import { Icon } from "rich/component/view/icon";
 class PagePublish extends EventsComponent {
     open(item: PageItem) {
         this.item = item;
@@ -15,15 +18,19 @@ class PagePublish extends EventsComponent {
         var self = this;
         function setGlobalShare(share: boolean) {
             self.item.share = share ? "net" : 'nas';
+            self.forceUpdate();
             workspaceService.updatePage(self.item.id, { share: self.item.share });
         }
         return <div className='shy-page-publish'>
-            <div>
-                <div>
-                    <GlobalLink></GlobalLink>
-                    <div><span>公开至互联网</span><label>任何人都可以浏览</label></div>
+            <div className='shy-page-publish-access'>
+                <div className='shy-page-publish-access-icon'>
+                    <Icon size={36} icon={GlobalLink}></Icon>
+                    <div>
+                        <span>公开至互联网</span>
+                        <label>任何人都可以浏览</label>
+                    </div>
                 </div>
-                <Switch checked={this.item.share == 'net' ? true : false} onChange={e => setGlobalShare(e)}></Switch>
+                <Switch checked={this.item?.share == 'net' ? true : false} onChange={e => setGlobalShare(e)}></Switch>
             </div>
             {/* <div>
                 分享给好友，一起参于
@@ -31,6 +38,13 @@ class PagePublish extends EventsComponent {
         </div>
     }
 }
-export async function useOpenPagePublish(pos: PopoverPosition, item: PageItem) {
-
+export async function usePagePublish(pos: PopoverPosition, item: PageItem) {
+    let popover = await PopoverSingleton(PagePublish, { mask: true });
+    let pagePublish = await popover.open(pos);
+    pagePublish.open(item);
+    return new Promise((resolve: (data: any) => void, reject) => {
+        popover.only('close', () => {
+            resolve(null)
+        });
+    })
 }
