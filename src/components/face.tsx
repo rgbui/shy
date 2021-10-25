ed4e6940f777b5a09868a1544b951f396f1ae7dd
kanhai <760489033@qq.com>
2021-10-25 21:20:51 +0800

    修改用户头像图标，支持圆角功能

--- src/components/face.tsx
+++ src/components/face.tsx
@@ -26,12 +26,12 @@ export class Avatar extends React.Component<{
             else {
                 return <>
                     {this.props.icon && <img style={{ width: size, height: size }} src={this.props.icon.url} />}
-                    {!this.props.icon && <span style={{ width: size, height: size, fontSize: size * 0.8, lineHeight: size + 'px' }}
+                    {!this.props.icon && <span style={{ width: size, height: size, fontSize: size * 0.6, lineHeight: (size * 0.6) + 'px' }}
                         className='shy-avatar-name'>{this.props.text.slice(0, 1)}</span>}
                 </>
             }
         }
-        return <div className='shy-avatar' onClick={e => {
+        return <div className={'shy-avatar' + (this.props.circle ? " shy-avatar-circle" : "")} onClick={e => {
             if (typeof this.props.onClick == 'function') this.props.onClick(e);
         }}>
             {renderIcon()}
