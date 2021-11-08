e4e764c2da72b1a57106cfcb8f9cfef214018844
kanhai <760489033@qq.com>
2021-11-08 22:10:31 +0800

    修复pageItem更新时，路由没有一块跟着更新的问题

--- src/view/surface/supervisor/bar/router.tsx
+++ src/view/surface/supervisor/bar/router.tsx
@@ -7,42 +7,25 @@ import { PageItem } from '../../sln/item';
 import { surface } from '../..';
 import PageSvg from "../../../../assert/svg/page.svg";
 import { Mime } from '../../sln/declare';
-export class PageRouter extends React.Component {
-    onClick(item: PageItem) {
-        if (this.supervisor.item === item) return;
+import { observer } from 'mobx-react';
+export var PageRouter = observer(function () {
+    function onClick(item) {
+        if (surface.supervisor.item === item) return;
         messageChannel.fire(Directive.OpenPageItem, item);
     }
-    componentDidMount() {
-        messageChannel.on(Directive.UpdatePageItem, this.changePageInfo);
+    var item = surface.supervisor.item;
+    if (item) {
+        var rootItem = item.closest(x => x && x.parent && x.parent.mime != Mime.page, true);
+        var pa = item.parent;
+        if (pa.mime != Mime.page) pa = null;
+        if (rootItem && rootItem == pa) pa = null;
+        var ra = (item: PageItem, split = false) => <><span onMouseDown={e => onClick(item)} className='shy-supervisor-bar-routers-item'><Icon icon={item.icon ? item.icon : PageSvg} size={18}></Icon><a className='shy-supervisor-bar-router-item-title'>{item.text}</a></span>{split && <em>/</em>}</>;
+        return <div className='shy-supervisor-bar-routers'>
+            {rootItem && ra(rootItem, true)}
+            {pa && pa.parent != rootItem && rootItem && <><label>...</label><em>/</em></>}
+            {pa != rootItem && pa && ra(pa, true)}
+            {ra(item)}
+        </div>
     }
-    componentWillUnmount() {
-        messageChannel.off(Directive.UpdatePageItem, this.changePageInfo);
-    }
-    changePageInfo = (id: string, pageInfo) => {
-        var pa = this.supervisor.item.closest(g => g.id == id);
-        if (pa) {
-            Object.assign(pa, pageInfo);
-            this.forceUpdate();
-        }
-    }
-    render() {
-        var item = this.supervisor.item;
-        if (item) {
-            var rootItem = item.closest(x => x && x.parent && x.parent.mime != Mime.page, true);
-            var pa = item.parent;
-            if (pa.mime != Mime.page) pa = null;
-            if (rootItem && rootItem == pa) pa = null;
-            var ra = (item: PageItem, split = false) => <><span onMouseDown={e => this.onClick(item)} className='shy-supervisor-bar-routers-item'><Icon icon={item.icon ? item.icon : PageSvg} size={18}></Icon><a className='shy-supervisor-bar-router-item-title'>{item.text}</a></span>{split && <em>/</em>}</>;
-            return <div className='shy-supervisor-bar-routers'>
-                {rootItem && ra(rootItem, true)}
-                {pa && pa.parent != rootItem && rootItem && <><label>...</label><em>/</em></>}
-                {pa != rootItem && pa && ra(pa, true)}
-                {ra(item)}
-            </div>
-        }
-        else return <></>;
-    }
-    get supervisor() {
-        return surface.supervisor;
-    }
-}
+    else return <></>;
+})
