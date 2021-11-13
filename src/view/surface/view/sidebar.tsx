b4475daf56f2c0202dc6e86c40f1ac4a69595df8
kanhai <760489033@qq.com>
2021-11-13 19:31:51 +0800

    实现user，workspace属性设置功能

--- src/view/surface/view/sidebar.tsx
+++ src/view/surface/view/sidebar.tsx
@@ -7,12 +7,12 @@ import { Avatar } from "../../../components/face";
 export var SideBar = observer(function () {
     return <div className='shy-sidebar'>
         <div className='shy-sidebar-top'>
-            <Avatar size={30} icon={surface.workspace.icon} text={surface.workspace.text}></Avatar>
+            <Avatar size={30} onClick={e => surface.workspace.onOpenWorkspaceSettings(e)} icon={surface.workspace.icon} text={surface.workspace.text}></Avatar>
             <Icon className='hover' size={40} fontSize={30} icon={"yingyong:sy"}></Icon>
         </div>
         <div className='shy-sidebar-bottom'>
             <Icon size={40} fontSize={30} icon={"help:sy"}></Icon>
-            <Avatar circle size={30} icon={surface.user.avatar} text={surface.user.name}></Avatar>
+            <Avatar circle size={30} onClick={e=>surface.workspace.onOpenUserSettings(e)} icon={surface.user.avatar} text={surface.user.name}></Avatar>
         </div>
     </div>
 })
