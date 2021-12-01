47193e557ab2c0a67607f19f2ccff493c40b73f7
kanhai <760489033@qq.com>
2021-12-02 00:04:59 +0800

    修改空间图标

--- src/view/surface/view/sidebar.tsx
+++ src/view/surface/view/sidebar.tsx
@@ -4,15 +4,16 @@ import React from "react";
 import { Icon } from "rich/component/view/icon";
 import { surface } from "..";
 import { Avatar } from "../../../components/face";
+import SvgComponents from "../../../assert/svg/components.svg";
 export var SideBar = observer(function () {
     return <div className='shy-sidebar'>
         <div className='shy-sidebar-top'>
             <Avatar size={30} onClick={e => surface.workspace.onOpenWorkspaceSettings(e)} icon={surface.workspace.icon} text={surface.workspace.text}></Avatar>
-            <Icon className='hover' size={40} fontSize={30} icon={"yingyong:sy"}></Icon>
+            <Icon className='hover' size={30} fontSize={30} icon={SvgComponents}></Icon>
         </div>
         <div className='shy-sidebar-bottom'>
             <Icon size={40} fontSize={30} icon={"help:sy"}></Icon>
-            <Avatar circle size={30} onClick={e=>surface.workspace.onOpenUserSettings(e)} icon={surface.user.avatar} text={surface.user.name}></Avatar>
+            <Avatar circle size={30} onClick={e => surface.workspace.onOpenUserSettings(e)} icon={surface.user.avatar} text={surface.user.name}></Avatar>
         </div>
     </div>
 })
