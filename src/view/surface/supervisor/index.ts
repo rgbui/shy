18debb7ee09fe06ce8dcc13b59ff751ff6b926a9
kanhai <760489033@qq.com>
2021-12-13 01:09:57 +0800

    增加文档属性菜单功能

--- src/view/surface/supervisor/index.ts
+++ src/view/surface/supervisor/index.ts
@@ -9,6 +9,16 @@ import { MenuItemType, MenuItemTypeValue } from "rich/component/view/menu/declar
 import { computed, makeObservable, observable } from "mobx";
 import { surface } from "..";
 import { createPageContent } from "./page";
+import SvgLink from "../../../assert/svg/link.svg";
+import SvgLock from "../../../assert/svg/lock.svg";
+import SvgVersion from "../../../assert/svg/versionHistory.svg";
+import SvgUndo from "../../../assert/svg/undo.svg";
+import CustomizePage from "../../../assert/svg/customizePage.svg";
+import SvgExport from "../../../assert/svg/file.svg";
+import Upload from "../../../assert/svg/import.svg";
+import SvgTrash from 'rich/src/assert/svg/trash.svg';
+import MoveTo from "rich/src/assert/svg/moveTo.svg";
+
 export class Supervisor extends Events {
     itemIds: string[] = [];
     pagesEl: HTMLElement;
@@ -27,8 +37,7 @@ export class Supervisor extends Events {
         return surface.workspace.find(g => g.id == id);
     }
     get items() {
-        if (this.item)
-            return [this.item]
+        if (this.item) return [this.item]
         else return [];
     }
     async onOpenItem(...items: PageItem[]) {
@@ -60,10 +69,26 @@ export class Supervisor extends Events {
     async onOpenPageProperty(event: React.MouseEvent) {
         var items: MenuItemType<string>[] = [
             { name: 'smallText', text: '小字号', type: MenuItemTypeValue.switch },
-            { name: 'fullWidth', text: '宽版', type: MenuItemTypeValue.switch }
+            { name: 'fullWidth', text: '宽版', type: MenuItemTypeValue.switch },
+            { type: MenuItemTypeValue.divide },
+            { name: 'layout', text: '版面', icon: CustomizePage, disabled: true },
+            { name: 'lock', text: '编辑保护', icon: SvgLock, disabled: true },
+            { type: MenuItemTypeValue.divide },
+            { name: 'favourite', icon: 'favorite:sy', text: '添加至收藏', disabled: true },
+            { name: 'history', icon: SvgVersion, text: '页面历史', disabled: true },
+            { name: 'copylink', icon: SvgLink, text: '复制链接', disabled: true },
+            { type: MenuItemTypeValue.divide },
+            { name: 'undo', text: '撤消', icon: SvgUndo, disabled: true },
+            { name: 'redo', text: '重做', icon: SvgUndo, disabled: true },
+            { name: 'delete', icon: SvgTrash, text: '删除', disabled: true },
+            { type: MenuItemTypeValue.divide },
+            { name: 'import', icon: Upload, text: '导入', disabled: true },
+            { name: 'export', text: '导出', icon: SvgExport, disabled: true, remark: '导出PDF,HTML,Markdown' },
+            { type: MenuItemTypeValue.divide },
+            { name: 'move', text: '移动', icon: MoveTo, disabled: true },
         ];
         while (true) {
-            var r = await useSelectMenuItem({ roundArea: Rect.fromEvent(event) }, items);
+            var r = await useSelectMenuItem({ roundArea: Rect.fromEvent(event) }, items,{overflow:'visible'});
             if (r?.item) {
                 if (r.item.name == 'smallText') {
                     // console.log(r.item.checked);
