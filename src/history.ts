93ce3e104c41b749842671666c4a90bf361aed8c
kanhai <760489033@qq.com>
2021-12-16 01:30:54 +0800

    增加UrlRoute，统一管制路由

--- src/history.ts
+++ src/history.ts
@@ -1,5 +1,6 @@
 import { createBrowserHistory } from "history";
-import { matchPath } from "react-router";
+import { generatePath, matchPath } from "react-router";
+import { config } from "./common/config";
 
 export const SyHistory = createBrowserHistory();
 
@@ -12,4 +13,63 @@ export function currentParams(routePath: string): Record<string, any> {
         return r.params;
     }
     return undefined;
+}
+
+export var UrlRoute = {
+    push(url: ShyUrl, state?: Record<string, any>) {
+        if (url == ShyUrl.workCreate && config.isPro) {
+            return SyHistory.push('https://shy.live' + url);
+        }
+        else if (url == ShyUrl.signIn) {
+            if (config.isPro && config.isUserWs) return SyHistory.push('https://shy.live' + url, state);
+        }
+        SyHistory.push(url, state);
+    },
+    redict(url: string | ShyUrl, state?: Record<string, any>) {
+        SyHistory.push(url, state)
+    },
+    pushToWs(sn: number) {
+        if (config.isPro) return SyHistory.push(`https://${sn}.shy.live/`);
+        else return SyHistory.push(ShyUrl.ws, { wsId: sn })
+    },
+    pushToPage(wsSn: number | string, pageSn: number) {
+        if (config.isPro) {
+            if (location.host == wsSn + '.shy.live') {
+                return SyHistory.push(this.gen(ShyUrl.page, { pageId: pageSn }));
+            }
+            else {
+                return SyHistory.push(`https://${wsSn}.shy.live` + this.gen(ShyUrl.page, { pageId: pageSn }));
+            }
+        }
+        else return SyHistory.push(this.gen(ShyUrl.pageDev, { wsId: wsSn, pageId: pageSn }))
+    },
+    isMatch(url: ShyUrl) {
+        return currentParams(url) ? true : false;
+    },
+    match(url: ShyUrl) {
+        return currentParams(url);
+    },
+    gen(url: ShyUrl, params: Record<string, any>) {
+        return generatePath(url, params);
+    }
+}
+
+export enum ShyUrl {
+    root = '/',
+    scene = '/scene',
+    wechat = '/wechat',
+    route = '/route',
+    privacy_protocol = '/privacy/protocol',
+    service_protocol = '/service/protocol',
+    download = '/download',
+    help = '/help',
+    signOut = '/sign/out',
+    signIn = '/sign/in',
+    shiyun = '/shiyun',
+    ws = '/ws/:wsId',
+    pageDev = '/ws/:wsId/page/:pageId',
+    page = '/page/:pageId',
+    invite = '/invite/:id',
+    workCreate = '/work/create',
+    _404 = '/404'
 }
