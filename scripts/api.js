81a1243f70d742de7f76a3c754b22202088fe75e
kanhai <760489033@qq.com>
2022-01-25 23:58:36 +0800

    修改api生成脚本

--- scripts/api.js
+++ scripts/api.js
@@ -10,9 +10,20 @@ function push(url, args, returnType, methods, remark) {
     else if (Array.isArray(returnType)) apis.push({ ...objectUrl, args, returnType: returnType[0], syncType: returnType[1], methods, remark });
 }
 
-function build(fileName, project) {
-    var ps = apis.filter(g => g.methods.some(s => s == project));
-    var strs = [];
+function build(fileName) {
+    var ps = apis;
+    var syncs = [];
+    var onlys = [];
+    var onces = [];
+    var offs = [];
+    var fires = [];
+    var puts = [];
+    var dels = [];
+    var posts = [];
+    var gets = [];
+    var querys = [];
+    var acts = [];
+    var airs = [];
     function rt(type, isAsync) {
         if (isAsync) return `Promise<${type || 'void'}>`
         else return type;
@@ -25,60 +36,87 @@ function build(fileName, project) {
     }
 
     function getP(args, isD) {
-        if (args) { return (isD ? "," : "") + `parameter:${args}` }
-        else return '';
+        if (args) return args;
+        else return 'any';
     }
     ps.forEach(p => {
         var isAsync = false;
         if (p.methods.some(s => ['get', 'put', 'del', 'post'].includes(s))) var isHttp = true;
         if (p.methods.includes('await')) isAsync = true;
         if (!isHttp && !p.methods.includes('query') && !p.methods.includes('act') || p.methods.includes('air')) {
-            strs.push(`sync(url:'${p.url}',handle:(${getP(p.args)})=>${rt(p.returnType, isAsync)});`);
-            strs.push(`only(url:'${p.url}',handle:(${getP(p.args)})=>${rt(p.returnType, isAsync)});`);
-            strs.push(`once(url:'${p.url}',handle:(${getP(p.args)})=>${rt(p.returnType, isAsync)});`);
-            strs.push(`off(url:'${p.url}',handle?:(${getP(p.args)})=>${rt(p.returnType, isAsync)});`);
-            strs.push(`fire(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            syncs.push(`"${p.url}":{args:(r:${getP(p.args)})=>${rt(p.returnType, isAsync)},returnType:void}`);
+            onlys.push(`"${p.url}":{args:(r:${getP(p.args)})=>${rt(p.returnType, isAsync)},returnType:void}`);
+            onces.push(`"${p.url}":{args:(r:${getP(p.args)})=>${rt(p.returnType, isAsync)},returnType:void}`);
+            offs.push(`"${p.url}":{args:(r:${getP(p.args)})=>${rt(p.returnType, isAsync)},returnType:void}`);
+            fires.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         }
         if (isHttp) isAsync = true;
         if (is('put', p))
-            strs.push(`put(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            puts.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         if (is('del', p))
-            strs.push(`del(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            dels.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         if (is('post', p))
-            strs.push(`post(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            posts.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         if (is('get', p))
-            strs.push(`get(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            gets.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         if (is('query', p))
-            strs.push(`query(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            querys.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         if (is('act', p))
-            strs.push(`act(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            acts.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         if (is('air', p))
-            strs.push(`air(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
+            airs.push(`"${p.url}":{args:${getP(p.args)},returnType:${rt(p.returnType, isAsync)}}`);
         // if (!isHttp) strs.push(`fire(url:'${p.url}'${getP(p.args, true)}):${rt(p.returnType, isAsync)};`);
 
     })
-    if (project == 'rich')
-        fs.writeFileSync(fileName, `
+
+    fs.writeFileSync(fileName, `
 import { Field } from "../blocks/table-store/schema/field";
-import { FieldType } from "../blocks/table-store/schema/field.type";
 import { TableSchema } from "../blocks/table-store/schema/meta";
+import { FieldType } from "../blocks/table-store/schema/type";
 import { LinkPage } from "../extensions/at/declare";
 import { IconArguments } from "../extensions/icon/declare";
 import { GalleryType, OuterPic } from "../extensions/image/declare";
 import { User } from "../src/types/user";
-export interface Channel {
-    ${strs.join("\n\t")}
+export interface ChannelSyncMapUrls {
+    ${syncs.join(",\n\t")}
+}
+export interface ChannelOnlyMapUrls {
+    ${onlys.join(",\n\t")}
+}
+export interface ChannelOnceMapUrls {
+    ${onces.join(",\n\t")}
+}
+export interface ChannelOffMapUrls {
+    ${offs.join(",\n\t")}
+}
+export interface ChannelFireMapUrls {
+    ${fires.join(",\n\t")}
+}
+export interface ChannelDelMapUrls {
+    ${dels.join(",\n\t")}
+}
+export interface ChannelPostMapUrls {
+    ${posts.join(",\n\t")}
+}
+export interface ChannelPutMapUrls {
+    ${puts.join(",\n\t")}
+}
+export interface ChannelGetMapUrls {
+    ${gets.join(",\n\t")}
+}
+export interface ChannelQueryMapUrls {
+    ${querys.join(",\n\t")}
+}
+export interface ChannelActMapUrls {
+    ${acts.join(",\n\t")}
+}
+export interface ChannelAirMapUrls {
+    ${airs.join(",\n\t")}
 }
     `);
-    else {
-        fs.writeFileSync(fileName, `
-export interface Channel {
-    ${strs.join("\n\t")}
-}
-            `);
-    }
-}
 
+}
+push('/log',`{type:"error"|"warn"|"info",message:string|Error}`,'void',['rich'])
 push('/upload/file', '{file:File,uploadProgress: (event: ProgressEvent) => void}', '{ ok: boolean, data: { url: string },warn:string }', ['post', 'shy'])
 push('/workspace/upload/file', '{file:File,uploadProgress: (event: ProgressEvent) => void}', '{ ok: boolean, data: { url: string },warn:string }', ['post', 'workspace', 'rich'])
 push('/workspace/upload/file/url', '{url:string}', '{ ok: boolean, data: { url: string },warn:string }', ['post', 'workspace', 'rich'])
@@ -97,7 +135,7 @@ push('/query/current/user', '', 'User', ['rich', 'query']);
 
 push('/workspace/query/schemas', '{page?:number,size?:number}', '{ok:boolean,data:{total:number,list:Partial<TableSchema>[],page:number,size:number},warn:string}', ['rich', 'get']);
 push('/schema/create', '{text:string,templateId?:string}', '{ ok: boolean, data: { schema:Partial<TableSchema> },warn:string }', ['put', 'workspace', 'rich']);
-push('/schema/query', '{id:string}', '{ok:boolean,data:Partial<TableSchema>,warn:string}', ['rich', 'get']);
+push('/schema/query', '{id:string}', '{ok:boolean,data:{schema:Partial<TableSchema>},warn:string}', ['rich', 'get']);
 push('/schema/field/add', '{schemaId:string,text:string,type:FieldType}', '{ok:boolean,data:{field:Partial<Field>},warn:string}', ['rich', 'put']);
 push('/schema/field/remove', '{schemaId:string,fieldId:string}', '{ok:boolean,warn:string}', ['rich', 'del']);
 push('/schema/field/update', '{schemaId:string,fieldId:string,data:Partial<Field>}', '{ok:boolean,data:{field:Partial<Field>},warn:string}', ['rich', 'post']);
@@ -113,11 +151,11 @@ push('/datastore/query/ids', '{schemaId:string,ids:string[]}', '{ok:boolean,data
 push('/datastore/query/all', '{schemaId:string,page?:number,size?:number,filter?:Record<string, any>,sorts?:Record<string, 1|-1>}', '{ok:boolean,data:{list:any[],total:number,page:number,size:number},warn:string}', ['rich', 'get']);
 push('/datastore/group', '{schemaId:string,page?:number,size?:number,filter?:Record<string, any>,sorts?:Record<string, 1|-1>,group:string}', '{ok:boolean,data:{list:any[],total:number,page:number,size:number},warn:string}', ['rich', 'get']);
 push('/datastore/statistics', '{schemaId:string,page?:number,size?:number,filter?:Record<string, any>,having?:Record<string, any>,sorts?:Record<string, 1|-1>,groups:string[],aggregate:string[]}', '{ok:boolean,data:{list:any[],total:number,page:number,size:number},warn:string}', ['rich', 'get']);
-push('/datastore/statistics/value', '{schemaId:string,filter:Record<string, any>,indicator:string[]}', '{ok:boolean,data:{value:number},warn:string}', ['rich', 'get']);
+push('/datastore/statistics/value', '{schemaId:string,filter?:Record<string, any>,indicator:string[]}', '{ok:boolean,data:{value:number},warn:string}', ['rich', 'get']);
 
 push('/device/register', '', 'void', ['shy', 'act', 'await']);
-push('/device/get', '', 'string', ['shy', 'query', 'await']);
-push('/user/ping', 'void', '{ok:boolean,warn:string}', ['shy', 'get', 'await'])
+push('/device/query', '', 'string', ['shy', 'query', 'await']);
+push('/user/ping', '', '{ok:boolean,warn:string}', ['shy', 'get', 'await'])
 
-build(path.join(__dirname, "../../rich/net/declare.d.ts"), 'rich');
-build(path.join(__dirname, "../net/declare.ts"), 'shy');
+build(path.join(__dirname, "../../rich/net/declare.ts"), 'rich');
+//build(path.join(__dirname, "../net/declare.ts"), 'shy');
