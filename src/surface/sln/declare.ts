8f780aa84669e51bde2a6ba5cdceada87d25f2ac
kanhai <760489033@qq.com>
2022-01-29 21:17:03 +0800

    Mime

--- src/surface/sln/declare.ts
+++ src/surface/sln/declare.ts
@@ -9,15 +9,17 @@ export enum SlnDirective {
 
 
 export enum Mime {
-    none=0,
+    none = 0,
     page = 10,
     pages = 20,
     favourite = 30,
     trash = 40,
     import = 50,
     inbox = 60,
-    table = 70,
+    tables = 70,
+    table = 71,
     files = 80,
+    folder = 81,
     template = 90,
     chatroom = 100
 }
