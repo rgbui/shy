b8a2ecb191b4129544a4560f9330f23ad50c8e22
kanhai <760489033@qq.com>
2022-02-11 23:23:24 +0800

    调整本地的存储数据库表结构

--- net/db/index.ts
+++ net/db/index.ts
@@ -1,29 +1,15 @@
 import { Dexie } from "dexie";
 export var db = new Dexie("shy");
-db.version(3).stores({
-    page_snapshoot: 'id,creater,createDate,page_url,sequence,begin_sequence,end_sequence,file,content',
-    page_current_sequence: 'id,creater,createDate,page_url,page_snapshoot_id,operator_sequence,page_sequence',
+db.version(4).stores({
+    view_snap: 'id,seq,file,content,creater,createDate',
     log: 'id,creater,createDate,level,msg,remark,isReport'
 });
-export interface page_snapshoot {
+
+export interface view_snap {
     id: string;
-    creater: string;
-    createDate: number;
-    page_url: string;
-    sequence: number;
-    begin_sequence: number;
-    end_sequence: number;
-    file?: string;
-    content?: Blob
-}
-export interface page_current_sequence {
-    id: string;
-    creater: string;
-    createDate: number;
-    page_url: string;
-    page_snapshoot_id: string;
-    operator_sequence: number;
-    page_sequence: number;
+    seq: number,
+    file: Blob,
+    content
 }
 export interface db_log {
     id: string;
