8d7e6bdafbea4b0f8d42f4819988baabab16a0c1
kanhai <760489033@qq.com>
2022-02-13 23:56:03 +0800

    增加viewSnap锁的一些判断

--- services/snap/sync.ts
+++ services/snap/sync.ts
@@ -1,20 +1,24 @@
 import { IconArguments } from "rich/extensions/icon/declare";
-import { SockResponse } from "rich/net/declare";
 import { ElementType, getElementUrl } from "rich/net/element.type";
+import { UserAction } from "rich/src/history/action";
 import { view_snap } from "../../net/db";
 import { DbService } from "../../net/db/service";
 import { timService } from "../../net/primus";
 import { surface } from "../../src/surface";
-import { XhrReadFileBlob } from "../../src/util/file";
 const DELAY_TIME = 1000 * 60 * 1;
 var snapSyncMaps: Map<string, SnapSync> = new Map();
+
+export type ViewOperate={
+    operate:UserAction,
+    seq:number
+}
 export class SnapSync {
     elementUrl: string;
     private constructor(elementUrl: string) { this.elementUrl = elementUrl; }
     get localId() {
         return '/' + surface.workspace.id + '/' + this.elementUrl
     }
-    async viewOperator(operate: {}) {
+    async viewOperator(operate: Partial<UserAction>) {
         var r = await surface.workspace.sock.put<{
             seq: number,
             id: string
@@ -29,16 +33,18 @@ export class SnapSync {
             return r.data;
         }
     }
-    private localViewSnap: { seq: number, content: Blob | string, date: Date };
+    private localViewSnap: { seq: number, content: string, date: Date };
     private localTime;
-    async viewSnap(seq: number, content: Blob | string) {
+    async viewSnap(seq: number, content: string) {
         /**
          * 本地先存起来
          */
         await new DbService<view_snap>('view_snap').save({ id: this.localId }, {
-            file: content instanceof Blob ? content : undefined,
-            content: !(content instanceof Blob) ? (typeof content == 'object' ? JSON.stringify(content) : content) : undefined,
-            seq
+            id: this.localId,
+            content,
+            seq,
+            creater: surface?.user?.id,
+            createDate: new Date()
         });
         this.localViewSnap = { seq, content, date: new Date() };
         if (this.localTime) clearTimeout(this.localTime);
@@ -54,25 +60,14 @@ export class SnapSync {
         if (this.localTime) { clearTimeout(this.localTime); this.localTime = undefined; }
         if (this.localViewSnap) {
             if (this.lastServiceViewSnap && this.lastServiceViewSnap.seq >= this.localViewSnap.seq) return;
-            var tryLocker = await surface.workspace.sock.get<{ lock: false, lockSockId: string }>('/view/snap/lock', {
+            var tryLocker = await surface.workspace.sock.get<{ lock: boolean, lockSockId: string }>('/view/snap/lock', {
                 elementUrl: this.elementUrl,
                 wsId: surface.workspace.id,
                 sockId: timService.tim.id,
                 seq: this.localViewSnap.seq
             });
-            if (tryLocker.ok && tryLocker.data.lock == false) {
-                var r: SockResponse<any, any>;
-                if (this.localViewSnap.content instanceof Blob)
-                    r = await surface.workspace.sock.upload(this.localViewSnap.content, {
-                        url: '/view/snap',
-                        data: {
-                            elementUrl: this.elementUrl,
-                            wsId: surface.workspace.id,
-                            sockId: timService.tim.id,
-                            seq: this.localViewSnap.seq
-                        }
-                    });
-                else r = await surface.workspace.sock.put('/view/snap', {
+            if (tryLocker.ok && tryLocker.data.lock == true) {
+                var r = await surface.workspace.sock.put('/view/snap', {
                     elementUrl: this.elementUrl,
                     wsId: surface.workspace.id,
                     sockId: timService.tim.id,
@@ -80,6 +75,9 @@ export class SnapSync {
                     content: this.localViewSnap.content
                 })
                 if (r.ok) {
+                    if (typeof this.lastServiceViewSnap == 'undefined') {
+                        this.lastServiceViewSnap = {} as any;
+                    }
                     this.lastServiceViewSnap.seq = this.localViewSnap.seq;
                     this.lastServiceViewSnap.date = new Date();
                 }
@@ -104,10 +102,8 @@ export class SnapSync {
             seq
         });
         if (r.ok) {
-            if (r.data.localExisting == true) return { file: local.file };
-            var file;
-            if (r.data.file?.url) file = await XhrReadFileBlob(r.data.file.url);
-            return { file, operates: r.data.operates, content: r.data.content }
+            if (r.data.localExisting == true) return { content: local.content ? JSON.parse(local?.content) : {} };
+            return { operates: r.data.operates as ViewOperate[], content: r.data.content ? JSON.parse(r.data.content) : {} }
         }
     }
     static create(type: ElementType, parentId: string, id?: string) {
