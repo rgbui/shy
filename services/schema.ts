63abd0b5bcc4697080432136ec0fe108f039f654
kanhai <760489033@qq.com>
2022-02-13 23:58:23 +0800

    移除field的一些操作

--- services/schema.ts
+++ services/schema.ts
@@ -3,7 +3,8 @@
 
 
 
-import { put, del, post, get } from "rich/net/annotation";
+import { put, get } from "rich/net/annotation";
+import { timService } from "../net/primus";
 import { surface } from "../src/surface";
 
 class SchemaService {
@@ -11,39 +12,19 @@ class SchemaService {
     async createSchema() {
         var data = { ...arguments[0] };
         data.workspaceId = surface.workspace.id;
-        return  await surface.workspace.sock.put('/schema/create', data);
+        return await surface.workspace.sock.put('/schema/create', data);
     }
     @get('/schema/query')
     async searchSchema() {
-        return  await surface.workspace.sock.get('/schema/query', arguments[0]);
+        return await surface.workspace.sock.get('/schema/query', arguments[0]);
     }
-    @put('/schema/field/add')
-    async createSchemaField() {
-        var data = { ...arguments[0] };
-        data.workspaceId = surface.workspace.id;
-        return  await surface.workspace.sock.put('/schema/field/add', data);
-    }
-
-    @del('/schema/field/remove')
-    async removeSchemaField() {
-        return  await surface.workspace.sock.delete('/schema/field/remove', arguments[0]);
-    }
-    /**
-     * 切换字段类型
-     * 主要是考虑到有些字段需要转换
-     * 如果数据量比较大，这个转换估计会有点慢，
-     * 有些直接转换不了，转换不了直接置为空或者取当前默认值
-     * @param id 
-     * @param name 
-     * @param type 
-     */
-
-    @post('/schema/field/turn')
-    async turnSchemaFieldType() {
-        return await surface.workspace.sock.post('/schema/field/turn', arguments[0]);
-    }
-    @post('/schema/field/update')
-    async updateSchemaField() {
-        return await  surface.workspace.sock.post('/schema/field/update', arguments[0]);
+    @put('/schema/operate')
+    async schemaOperate(args: {  operate: Record<string, any> }) {
+        return await surface.workspace.sock.put<{ result: { actions: any[] } }>('/view/operate/sync', {
+            wsId: surface.workspace.id,
+            operate: args.operate,
+            schema: 'DataGridSchema',
+            sockId: timService.tim.id
+        })
     }
 }
