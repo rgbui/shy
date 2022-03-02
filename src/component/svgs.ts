16b686c9372af761462a47f45b3a1b70fcdb6036
kanhai <760489033@qq.com>
2022-03-02 23:16:41 +0800

    生成shy 中的svgs

---
+++ src/component/svgs.ts
@@ -0,0 +1,47 @@
+import BillingSvg from "../assert/svg/billing.svg";
+import ComponentsSvg from "../assert/svg/components.svg";
+import ConsoleSvg from "../assert/svg/console.svg";
+import CustomizePageSvg from "../assert/svg/customizePage.svg";
+import CutSvg from "../assert/svg/cut.svg";
+import DoubleRightSvg from "../assert/svg/doubleRight.svg";
+import DownloadSvg from "../assert/svg/download.svg";
+import ExpandSvg from "../assert/svg/expand.svg";
+import FileSvg from "../assert/svg/file.svg";
+import FriendsSvg from "../assert/svg/friends.svg";
+import ImportSvg from "../assert/svg/import.svg";
+import LinkSvg from "../assert/svg/link.svg";
+import LockSvg from "../assert/svg/lock.svg";
+import MemberSvg from "../assert/svg/member.svg";
+import MenuSvg from "../assert/svg/menu.svg";
+import PageSvg from "../assert/svg/page.svg";
+import PubWorkspaceSvg from "../assert/svg/pubWorkspace.svg";
+import RenameSvg from "../assert/svg/rename.svg";
+import SystemSvg from "../assert/svg/system.svg";
+import UndoSvg from "../assert/svg/undo.svg";
+import VersionHistorySvg from "../assert/svg/versionHistory.svg";
+import WsSettingsSvg from "../assert/svg/wsSettings.svg";
+
+export  {
+BillingSvg,
+ComponentsSvg,
+ConsoleSvg,
+CustomizePageSvg,
+CutSvg,
+DoubleRightSvg,
+DownloadSvg,
+ExpandSvg,
+FileSvg,
+FriendsSvg,
+ImportSvg,
+LinkSvg,
+LockSvg,
+MemberSvg,
+MenuSvg,
+PageSvg,
+PubWorkspaceSvg,
+RenameSvg,
+SystemSvg,
+UndoSvg,
+VersionHistorySvg,
+WsSettingsSvg
+}
