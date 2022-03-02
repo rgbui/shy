dbec49c9933f4a8b2065860e8153b0e2032277e2
kanhai <760489033@qq.com>
2022-03-03 01:25:57 +0800

    实现添加好友功能

--- src/component/join.tsx
+++ src/component/join.tsx
@@ -1,8 +1,11 @@
+import { Alert } from "rich/component/lib/alert";
 import { EventsComponent } from "rich/component/lib/events.component";
 import { Button } from "rich/component/view/button";
 import { Input } from "rich/component/view/input";
+import { ErrorText, Remark } from "rich/component/view/text";
 import { PopoverSingleton } from "rich/extensions/popover/popover";
 import { PopoverPosition } from "rich/extensions/popover/position";
+import { channel } from "rich/net/channel";
 import React from "rich/node_modules/@types/react";
 
 class JoinFriend extends EventsComponent {
@@ -10,14 +13,41 @@ class JoinFriend extends EventsComponent {
         return <div className="shy-join-friend">
             <div className="shy-join-friend-head"><span>添加好友</span></div>
             <div className="shy-join-friend-content">
-                <Input value={this.name} onEnter={e => this.sendFriend()} onChange={e => this.name = e}></Input>
-                <Button onClick={e => this.sendFriend()}>发送好友请求</Button>
+                <Remark>请输入用户的诗云号(数字）</Remark>
+                <div>
+                    <Input value={this.name} onEnter={e => this.sendFriend()} onChange={e => this.name = e}></Input>
+                    <Button ref={e => this.button = e} onClick={e => this.sendFriend()}>发送好友请求</Button>
+                </div>
+                <div>
+                    {this.error && <ErrorText >{this.error}</ErrorText>}
+                </div>
             </div>
         </div>
     }
     name: string = '';
-    sendFriend() {
-
+    error: string = '';
+    button: Button;
+    async sendFriend() {
+        this.error = '';
+        this.forceUpdate();
+        if (this.name && /^[\d]+$/g.test(this.name)) {
+            this.button.loading = true;
+            var r = await channel.put('/friend/join', { sn: parseInt(this.name) });
+            this.button.loading = false;
+            if (r.ok) {
+                if (r.data.exists) {
+                    await Alert('好友请求已发送')
+                }
+                else if (r.data.exists == false) {
+                    await Alert('帐号不存在')
+                }
+                else if (r.data.send) {
+                    this.emit('close')
+                }
+            }
+        }
+        else this.error = '请输入合法的诗云号';
+        this.forceUpdate();
     }
     open() {
 
