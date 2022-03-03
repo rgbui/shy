7e531c37631f0582fd43feb25d261fc385b63a00
kanhai <760489033@qq.com>
2022-03-03 23:59:04 +0800

    实现好友的待发送管理

--- src/surface/user/channel/friends/index.tsx
+++ src/surface/user/channel/friends/index.tsx
@@ -16,30 +16,31 @@ export var FriendsView = observer(function () {
         }
     })
     function setMode(mode: string) {
-
+        local.mode = mode;
     }
     async function joinFriend(event: React.MouseEvent) {
         await useJoinFriend({ roundArea: Rect.fromEvent(event) })
     }
     return <div className="shy-user-channel-friends">
-        <div className="shy-user-channel-friends-head">
-            <div>
+        <div className="shy-user-channel-friends-head flex-auto-fix">
+            <div className="shy-user-channel-friends-head-tabs">
                 <Icon icon={FriendSvg}></Icon>
                 <span>好友</span>
+                <div className="line"></div>
                 <a onMouseDown={e => setMode('online')} className={local.mode == 'online' ? "hover" : ""}>在线</a>
                 <a onMouseDown={e => setMode('all')} className={local.mode == 'all' ? "hover" : ""}>全部</a>
                 <a onMouseDown={e => setMode('pending')} className={local.mode == 'pending' ? "hover" : ""}>待定</a>
                 <a onMouseDown={e => setMode('shield')} className={local.mode == "shield" ? "hover" : ""}>屏蔽</a>
-                <Button onClick={e => joinFriend(e)}>添加好友</Button>
+                <Button size="small" onClick={e => joinFriend(e)}>添加好友</Button>
             </div>
-            <div>
+            <div className="shy-user-channel-friends-head-btns" style={{ width: 80 }}>
                 <Icon size={40} fontSize={30} icon={"help:sy"}></Icon>
             </div>
         </div>
         <div className="shy-user-channel-friends-content">
-            {(this.mode == 'online' || this.mode == 'all') && <FrendListView></FrendListView>}
-            {this.mode == 'pending' && <PendListView></PendListView>}
-            {this.mode == 'shield' && <BlackListView></BlackListView>}
+            {(local.mode == 'online' || local.mode == 'all') && <FrendListView></FrendListView>}
+            {local.mode == 'pending' && <PendListView></PendListView>}
+            {local.mode == 'shield' && <BlackListView></BlackListView>}
         </div>
     </div>
 })
--- src/surface/user/channel/friends/pends.tsx
+++ src/surface/user/channel/friends/pends.tsx
@@ -3,6 +3,10 @@ import { observer, useLocalObservable } from "mobx-react";
 import { Avatar } from "rich/component/view/avator/face";
 import { channel } from "rich/net/channel";
 import React from "react";
+import { surface } from "../../..";
+import { Icon } from "rich/component/view/icon";
+import { CheckSvg, CloseTickSvg } from "rich/component/svgs";
+import { Input } from "rich/component/view/input";
 
 export var PendListView = observer(function () {
     var local = useLocalObservable(() => {
@@ -13,11 +17,27 @@ export var PendListView = observer(function () {
             total: 0
         }
     })
+    async function removeSend(row) {
+        var r = await channel.del('/friend/delete', { id: row.id });
+        if (r.ok) {
+            local.list.remove(g => g.id == row.id);
+            local.list = local.list;
+        }
+    }
+    async function agree(row) {
+        var r = await channel.put('/friend/agree', { id: row.id });
+        if (r.ok) {
+            local.list.remove(g => g.id == row.id);
+            local.list = local.list;
+        }
+    }
     async function load() {
-        var r = await channel.get('/friends/pending', { page: 1, size: 200 });
+        var r = await channel.get('/friends/pending', { page: local.page, size: local.size });
         if (r.ok) {
             runInAction(() => {
-                Object.assign(local, r.data);
+                for (let n in r.data) {
+                    local[n] = r.data[n];
+                }
             })
         }
     }
@@ -25,11 +45,27 @@ export var PendListView = observer(function () {
         load();
     }, [])
     return <div className="shy-pendlist">
-        {local.list.map(r => {
-            return <div key={r.id}>
-                <Avatar userid={r.friendId}></Avatar>
-                <div className="shy-blacklist-operator"></div>
-            </div>
-        })}
+        <div className="shy-pendlist-search"><Input placeholder="搜索" /></div>
+        <div className="shy-pendlist-head"><span>待处理数{local.total}</span></div>
+        <div className="shy-pendlist-list">
+            {local.list.map(r => {
+                return <div key={r.id} className='shy-pendlist-user'>
+                    {r.userid == surface.user?.id && <><Avatar circle size={40} userid={r.friendId}></Avatar>
+                        <span>已发送好友请求</span>
+                        <div className="shy-pendlist-user-operator">
+                            <Icon size={24} wrapper icon={CloseTickSvg} click={e => removeSend(r)}></Icon>
+                        </div>
+                    </>
+                    }
+                    {r.friendId == surface.user?.id && <><Avatar circle size={40} userid={r.friendId}></Avatar>
+                        <span>来自ta的好友请求</span>
+                        <div className="shy-pendlist-user-operator">
+                            <Icon size={24}  wrapper  icon={CheckSvg} click={e => agree(r)}></Icon>
+                        </div>
+                    </>}
+                </div>
+            })}
+        </div>
+
     </div>
 });
