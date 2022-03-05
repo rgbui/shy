88db30a912f1c7178cff923e23ce359a934cf4d0
kanhai <760489033@qq.com>
2022-03-05 23:01:41 +0800

    增加对用户查询数据的本地缓存

---
+++ native/store/user.ts
@@ -0,0 +1,26 @@
+import { UserBasic } from "rich/types/user";
+
+
+class UserStore {
+    private ms: Map<string, UserBasic> = new Map();
+    async get(userid: string) {
+        return this.ms.get(userid);
+    }
+    async getUsers(...userids: string[]) {
+        var us: UserBasic[] = [];
+        await userids.eachAsync(async (id) => {
+            var r = await this.get(id);
+            if (r) us.push(r)
+        });
+        return us;
+    }
+    async put(user: UserBasic) {
+        var r = await this.get(user.id);
+        if (r) {
+            Object.assign(r, user);
+        }
+        else this.ms.set(user.id, user);
+    }
+}
+
+export var userNativeStore = new UserStore();
