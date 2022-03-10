b2848d17cca6b3fbf681f87ef484640d5f8d4675
kanhai <760489033@qq.com>
2022-03-10 23:07:30 +0800

    实现user profile功能

--- src/surface/user/profile/index.tsx
+++ src/surface/user/profile/index.tsx
@@ -2,14 +2,19 @@ import { observer } from "mobx-react";
 import React from "react";
 import { SettingsSvg } from "rich/component/svgs";
 import { Avatar } from "rich/component/view/avator/face";
-import { Icon } from "rich/component/view/icon";
+import { IconButton } from "rich/component/view/icon";
 import { surface } from "../..";
+import { useOpenUserSettings } from "../settings";
+import './style.less';
 
 export var UserProfile = observer(function () {
+    async function Mousedown(event: React.MouseEvent) {
+        var r = useOpenUserSettings()
+    }
     return <div className="shy-user-profile">
-        {surface.user && <Avatar userid={surface.user.id}></Avatar>}
+        {surface.user && <Avatar size={32} userid={surface.user.id}></Avatar>}
         <div className="shy-user-profile-operators">
-            <Icon icon={SettingsSvg}></Icon>
+            <IconButton icon={SettingsSvg} wrapper onMouseDown={e => Mousedown(e)} width={32} size={16}></IconButton>
         </div>
     </div>
 });
