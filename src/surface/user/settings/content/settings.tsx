c7f30de4163b731add2495ff99031142e09786bd
kanhai <760489033@qq.com>
2022-03-17 01:36:24 +0800

    实现用户的属性编辑功能

--- src/surface/user/settings/content/settings.tsx
+++ src/surface/user/settings/content/settings.tsx
@@ -3,9 +3,13 @@ import { Button } from 'rich/component/view/button';
 import { Row, Col, Divider } from 'rich/component/view/grid';
 import { surface } from '../../..';
 import { observer } from 'mobx-react';
-import { makeObservable, observable } from 'mobx';
+import { makeObservable, observable, runInAction } from 'mobx';
 import { User } from '../../user';
 import { channel } from 'rich/net/channel';
+import { useModifyName } from '../../common/setName';
+import { useUpdateEmail } from '../../common/email';
+import { useUpdatePhone } from '../../common/phone';
+import { useUpdatePaw } from '../../common/setPaw';
 
 @observer
 export class UserSettingsView extends React.Component<{ close?: () => void, setMode(): void }> {
@@ -23,15 +27,42 @@ export class UserSettingsView extends React.Component<{ close?: () => void, setM
             }
         })
     }
+    async modifyUserName(event: React.MouseEvent) {
+        await useModifyName()
+    }
+    async modifyEmail(event: React.MouseEvent) {
+        var r = await useUpdateEmail({ email: this.dataUser.email });
+        if (r) {
+            runInAction(() => {
+                this.dataUser.email = r;
+                this.dataUser.checkEmail = true;
+            })
+        }
+    }
+    async modifyPhone(event: React.MouseEvent) {
+        var r = await useUpdatePhone({ phone: this.dataUser.phone });
+        if (r) {
+            runInAction(() => {
+                this.dataUser.phone = r;
+                this.dataUser.checkPhone = true;
+            })
+        }
+    }
+    async modifyPwd(event: React.MouseEvent) {
+        var r = await useUpdatePaw({ checkPaw: this.dataUser.checkPaw });
+        if (r) {
+            this.dataUser.checkPaw = true;
+        }
+    }
     render() {
         return <div>
             <h1>我的帐号</h1>
-            <div className="shy-user-settings-profile-box-card" style={{ margin: 20 }}>
+            <div className="shy-user-settings-profile-box-card settings" style={{ margin: 20 }}>
                 <div className="bg">
-                    {!surface.user.cover?.url && <div style={{ height: 60, backgroundColor: surface.user?.cover?.color ? surface.user?.cover?.color : 'rgb(192,157,156)' }}></div>}
-                    {surface.user.cover?.url && <img style={{ height: 120 }} src={surface.user.cover?.url} />}
+                    {!surface.user.cover?.url && <div style={{ height: 100, backgroundColor: surface.user?.cover?.color ? surface.user?.cover?.color : 'rgb(192,157,156)' }}></div>}
+                    {surface.user.cover?.url && <img style={{ height: 180 }} src={surface.user.cover?.url} />}
                 </div>
-                <div className='shy-settings-user-avatar' style={{ top: surface.user.cover?.url ? 120 : 60 }}>
+                <div className='shy-settings-user-avatar' style={{ top: surface.user.cover?.url ? 180 : 100 }}>
                     {surface.user?.avatar && <img src={surface.user.avatar.url} />}
                     {!surface.user?.avatar && <span>{surface.user.name.slice(0, 1)}</span>}
                 </div>
@@ -42,23 +73,22 @@ export class UserSettingsView extends React.Component<{ close?: () => void, setM
                 <div className="shy-user-settings-profile-box-card-options">
                     <Row>
                         <Col span={24}><span>用户名</span></Col>
-                        <Col span={12}>{surface.user?.name}#{surface.user?.sn}</Col><Col span={12} align={'end'}><Button>编辑</Button></Col>
+                        <Col span={12}>{surface.user?.name}#{surface.user?.sn}</Col><Col span={12} align={'end'}><Button onClick={e => this.modifyUserName(e)}>编辑</Button></Col>
                     </Row>
                     <Row>
                         <Col span={24}><span>电子邮箱</span></Col>
-                        <Col span={12}><span>{this.dataUser?.email || '您还没有添加邮箱'}</span></Col><Col span={12} align={'end'}><Button>{this.dataUser?.checkEmail ? '新增' : '更换'}</Button></Col>
+                        <Col span={12}><span>{this.dataUser?.email || '您还没有添加邮箱'}</span></Col><Col span={12} align={'end'}><Button onClick={e => this.modifyEmail(e)}>{this.dataUser?.checkEmail ? '新增' : '更换'}</Button></Col>
                     </Row>
                     <Row>
                         <Col span={24}><span>手机号</span></Col>
-                        <Col span={12}><span>{this.dataUser?.phone || '您还没有添加手机号'}</span></Col><Col span={12} align={'end'}><Button>{this.dataUser?.checkPhone ? '新增' : '更换'}</Button></Col>
+                        <Col span={12}><span>{this.dataUser?.phone || '您还没有添加手机号'}</span></Col><Col span={12} align={'end'}><Button onClick={e => this.modifyPhone(e)}>{this.dataUser?.checkPhone ? '新增' : '更换'}</Button></Col>
                     </Row>
                 </div>
             </div>
             <Divider></Divider>
             <Row>
                 <Col><h5>密码</h5></Col>
-                <Col><Button>{this.dataUser?.checkPaw ? "修改密码" : "设置密码"}</Button>
-                </Col>
+                <Col><Button onClick={e => this.modifyPwd(e)}>{this.dataUser?.checkPaw ? "修改密码" : "设置密码"}</Button></Col>
             </Row>
             {/*<Row>
                 <Col><h5>删除帐户</h5></Col>
