530dfba0db57c516177ab2048d28a794bc746656
kanhai <760489033@qq.com>
2022-04-19 23:58:55 +0800

    增加权限枚举

---
+++ src/surface/workspace/permission.ts
@@ -0,0 +1,23 @@
+
+
+export enum WorkspacePermission {
+    /**
+     * 是否可以编辑文档
+     */
+    editDoc = 100,
+    /**
+     * 创建文档
+     * 删除文档
+     */
+    createOrDeleteDoc = 101,
+
+    /**
+     * 允许发言
+     */
+    sendMessageByChannel = 110,
+    /***
+     * 允许创建或删除文本频道
+     */
+    createOrDeleteChannel = 111,
+    
+}
