6fae8589246de59ea60e7c66ac12e4a13ac33d08
kanhai <760489033@qq.com>
2022-04-22 01:08:17 +0800

    实现空间的自定义域名设置功能

--- src/surface/workspace/settings/settings.tsx
+++ src/surface/workspace/settings/settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'rich/component/view/button';
-import { Row, Col, Divider } from 'rich/component/view/grid';
+import { Row, Col, Divider, Space } from 'rich/component/view/grid';
 import { Input, Textarea } from 'rich/component/view/input';
 import { Workspace } from '..';
 import { OpenFileDialoug } from 'rich/component/file';
@@ -8,6 +8,8 @@ import { surface } from '../..';
 import "./style.less";
 import { observer } from 'mobx-react';
 import { channel } from 'rich/net/channel';
+import { useSetWsDomain } from '../../user/common/setDomain';
+import { Remark } from 'rich/component/view/text';
 
 @observer
 export class WorkspaceSettingsView extends React.Component {
@@ -31,6 +33,12 @@ export class WorkspaceSettingsView extends React.Component {
                     surface.workspace.onUpdateInfo({ cover: { name: 'image', url: r.data.file.url } })
                 }
             }
+        }
+    }
+    async openDomain(event: React.MouseEvent) {
+        var r = await useSetWsDomain(surface.workspace.id, '');
+        if (r) {
+            surface.workspace.siteDomain = r;
         }
     }
     data: Partial<Workspace> = {
@@ -68,19 +76,28 @@ export class WorkspaceSettingsView extends React.Component {
     }
     render() {
         return <div>
-            <h1>工作空间</h1>
+            <h2>工作空间</h2>
+            <Divider></Divider>
             <Row>
-                <Col span={12}><h5>空间头像</h5></Col>
-                <Col span={12}><h5>空间横幅背景</h5></Col>
+                <Col span={12}><h5 style={{ marginBottom: 10 }}>空间头像</h5></Col>
+                <Col span={12}><h5 style={{ marginBottom: 10 }}>空间横幅背景</h5></Col>
                 <Col span={12}>
-                    <div className='shy-settings-ws-avatar' onClick={() => this.onUploadFace()} >
-                        {surface.workspace.icon && <img src={surface.workspace.icon.url} />}
-                        {!surface.workspace.icon && <span>{surface.workspace.text.slice(0, 1)}</span>}
-                    </div>
+                    <Space>
+                        <div className='shy-settings-ws-avatar' onClick={() => this.onUploadFace()} >
+                            {surface.workspace.icon && <img src={surface.workspace.icon.url} />}
+                            {!surface.workspace.icon && <span>{surface.workspace.text.slice(0, 1)}</span>}
+                            <div className='shy-settings-ws-avatar-hover'>添加图片</div>
+                        </div>
+                        <div>
+                            <p>我们建议使用至少 512x512 大小的图片。</p>
+                            <Button ghost onClick={e => this.onUploadFace()}>上传图片</Button>
+                        </div>
+                    </Space>
                 </Col>
                 <Col span={12}>
                     <div className='shy-settings-ws-cover' onClick={() => this.onUploadCover()} >
                         {surface.workspace.cover && <img src={surface.workspace.cover.url} />}
+                        <div className='shy-settings-ws-cover-hover'>更换横幅</div>
                     </div>
                     {!surface.workspace.cover && <Button onClick={() => this.onUploadCover()}>上传横幅背景</Button>}
                 </Col>
@@ -88,13 +105,13 @@ export class WorkspaceSettingsView extends React.Component {
             <Divider></Divider>
             <Row>
                 <Col span={24}><h5>工作空间名称</h5></Col>
-                <Col span={24}><label>点击输入框可修改名称</label></Col>
+                <Col span={24}><Remark>点击输入框可修改名称</Remark></Col>
                 <Col span={24}><Input value={this.data.text || surface.workspace.text} onChange={e => this.setData({ text: e })} placeholder={'请输入你的工作空间名称'}></Input></Col>
             </Row>
             <Divider></Divider>
             <Row>
                 <Col span={24}><h5>工作描述</h5></Col>
-                <Col span={24}><label>点击输入框可修改名称</label></Col>
+                <Col span={24}><Remark>点击输入框可修改名称</Remark></Col>
                 <Col span={24}><Textarea value={this.data.slogan || surface.workspace.slogan} onChange={e => this.setData({ text: e })} placeholder={'请输入你的工作空间名称'}></Textarea></Col>
             </Row>
             <Divider></Divider>
@@ -103,7 +120,7 @@ export class WorkspaceSettingsView extends React.Component {
                 <Col>
                     <div className='shy-ws-settings-view-domain'>
                         <a style={{ textDecoration: 'underline', color: 'inherit' }} href={'https://' + (surface.workspace.siteDomain || surface.workspace.sn) + '.shy.live'}>https://{surface.workspace.siteDomain || surface.workspace.sn}.shy.live</a>
-                        <Button link>更换空间域名</Button>
+                        {!surface.workspace.siteDomain && <Button onClick={e => this.openDomain(e)} link>更换空间域名</Button>}
                     </div>
                 </Col>
             </Row>
