0579bab07fa70fc06347f3c2d109d0c759579809
kanhai <760489033@qq.com>
2022-04-21 23:01:30 +0800

    clear code

--- src/surface/user/settings/content/update.tsx
+++ src/surface/user/settings/content/update.tsx
@@ -6,8 +6,8 @@ export class ShyAppUpdate extends React.Component {
     render() {
         return <div className="shy-app-update">
             <h2>更新日志</h2>
-            <Remark>记录着我们成长的脚步，也记录着大家对我们的期望</Remark>
             <Divider></Divider>
+            <Remark>记录着我们成长的脚步，也记录着大家对我们的期望</Remark>
             <div className="shy-app-update-content" dangerouslySetInnerHTML={{ __html: md }}></div>
         </div>
     }
