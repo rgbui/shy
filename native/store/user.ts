1e8ffbcf3b7df87c0cc841fdd3f9f26b5b2c200a
kanhai <760489033@qq.com>
2022-04-23 17:32:42 +0800

    修复保存的user可能是空的，导致报错

--- native/store/user.ts
+++ native/store/user.ts
@@ -1,6 +1,5 @@
 import { UserBasic } from "rich/types/user";
 
-
 class UserStore {
     private ms: Map<string, UserBasic> = new Map();
     async get(userid: string) {
@@ -15,6 +14,7 @@ class UserStore {
         return us;
     }
     async put(user: Partial<UserBasic>) {
+        if (!user) return;
         var r = await this.get(user.id);
         if (r) {
             Object.assign(r, user);
