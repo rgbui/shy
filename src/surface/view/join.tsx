3e5ecd4518b77e76dfe5e7952bafd0c72a851a97
kanhai <760489033@qq.com>
2022-05-16 20:30:46 +0800

    实现加入空间的提示

---
+++ src/surface/view/join.tsx
@@ -0,0 +1,20 @@
+import { observer } from "mobx-react";
+import React from "react";
+import { channel } from "rich/net/channel";
+import { surface } from "..";
+export var JoinTip = observer(function () {
+    async function mousedown(event: React.MouseEvent) {
+        var joinM = await channel.put('/user/join/ws', {
+            wsId: surface.workspace.id
+        });
+        var r = await channel.put('/ws/invite/join', {
+            wsId: surface.workspace.id,
+            sock: surface.workspace.sock
+        });
+        await surface.loadWorkspace(surface.workspace.id);
+    }
+    return <div className='shy-supervisor-join-tip' >
+        <span>您当前处于预览模式，加入该空间一起协作吧</span>
+        <a onMouseDown={e => mousedown(e)}>加入&nbsp;{surface.workspace?.text}</a>
+    </div>
+})
