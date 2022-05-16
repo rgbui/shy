89e2626c69f83310796177f3ae728d0f514f891e
kanhai <760489033@qq.com>
2022-05-16 22:30:04 +0800

    修复空间邀请链接复制无效的问题

--- src/surface/workspace/settings/invite.tsx
+++ src/surface/workspace/settings/invite.tsx
@@ -10,29 +10,33 @@ import { surface } from "../..";
 
 @observer
 export class WorkspaceInvite extends React.Component {
-    async createInvite() {
-        if (!surface.workspace.invite) {
+    async createInvite(force?: boolean) {
+        if (force == true || !surface.workspace.invite) {
             var r = await channel.put('/ws/invite/create');
             if (r.ok) {
                 surface.workspace.invite = r.data.code;
-                this.forceUpdate();
             }
         }
         var url = location.protocol + '//' + location.host + '/invite/' + surface.workspace.invite
         CopyText(url);
-        alert('邀请链接已复制')
+        if (this.input) this.input.updateValue(url);
+        setTimeout(() => {
+            alert('邀请链接已复制')
+        },500);
     }
+    input: Input;
     render() {
         return <div className='shy-ws-invites'>
             <h2>邀请</h2>
             <Divider></Divider>
+            <h3 style={{ fontSize: 14 }}>通用的邀请链接</h3>
             <Row>
-                <Remark>任何得到此链接的人均可以加入这个空间。</Remark>
+                <Remark>任何得到此链接的人均可以加入这个空间(空间没有人数上限的限制)，你也可以<a onMouseDown={e => this.createInvite(true)}>重置链接</a></Remark>
             </Row>
             <Row>
-                <Col span={16}><Input
+                <Col span={16}><Input ref={e => this.input = e}
                     readonly={true} value={surface.workspace.invite ? location.protocol + '//' + location.host + '/invite/' + surface.workspace.invite : ''}></Input></Col>
-                <Col align='start' span={8}><Button style={{ marginLeft: 20 }}
+                <Col align='start' span={8}><Button size={'larger'} style={{ marginLeft: 20 }}
                     onClick={e => this.createInvite()}
                     ghost>{surface.workspace.invite ? '复制链接' : '创建链接'}</Button></Col>
             </Row>
