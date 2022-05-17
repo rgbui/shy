5c4888462df8b9dd762c4a8179d20ba7d9562b69
kanhai <760489033@qq.com>
2022-05-17 11:41:35 +0800

    调整404页面显示效果

--- src/site/404.tsx
+++ src/site/404.tsx
@@ -1,5 +1,12 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 export var View404 = observer(function () {
-    return <div className='shy-404'>404</div>
+    return <div className='shy-404'>
+        <div className='shy-404-content'>
+            <h3 style={{ textAlign: 'center', fontSize:120 }}>404</h3>
+            <div className="shy-404-content-text">
+                <span>当前的页面不存在,返回至<a href='https://shy.live'>诗云</a></span>
+            </div>
+        </div>
+    </div>
 })
