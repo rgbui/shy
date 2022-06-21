f29495ae323dfe62d0f7c50a561bcab0248146ef
rgbui <760489033@qq.com>
2022-06-21 13:04:04 +0800

    sidebar

--- src/surface/view/sidebar.tsx
+++ src/surface/view/sidebar.tsx
@@ -22,20 +22,22 @@ export var SideBar = observer(function () {
         else return <a className="shy-sidebar-ws-name"><span style={{ fontSize: 18 }}>{workspace?.text?.slice(0, 2)}</span></a>
     }
     return <div className='shy-sidebar'>
-        <a className="shy-sidebar-operator" style={{ marginTop: config.isPc && UA.isMacOs ? 30 : 0 }} onMouseDown={e => {
-            //  UrlRoute.push(ShyUrl.me)
+        <a className="shy-sidebar-operator" style={{ marginTop: config.isPc && UA.isMacOs ? 30 : 20 }} onMouseDown={e => {
+            UrlRoute.push(ShyUrl.me)
         }
         }>
             <img src={HomeSrc} style={{ width: 48, height: 48, borderRadius: 16 }} />
         </a>
         <div className="shy-sidebar-divider"></div>
-        {surface.temporaryWs && <div onMouseDown={e => surface.onChangeWorkspace(surface.temporaryWs)} key={surface.temporaryWs.id} className={'shy-sidebar-ws' + (surface.workspace.id == surface.temporaryWs.id ? " hover" : "")}>{renderWs(surface.temporaryWs)}</div>}
-        {surface.wss.map(ws => {
-            return <div onMouseDown={e => surface.onChangeWorkspace(ws)} key={ws.id} className={'shy-sidebar-ws' + (surface.workspace?.id == ws.id ? " hover" : "")}>{renderWs(ws)}</div>
-        })}
-        <a className="shy-sidebar-operator" onMouseDown={e => surface.onCreateWorkspace()} ><Icon size={24} icon={PlusSvg}></Icon></a>
-        <a className="shy-sidebar-operator" onMouseDown={e => UrlRoute.push(ShyUrl.discovery)}><Icon size={24} icon={PubWorkspace}></Icon></a>
-        {/*  <div className="shy-sidebar-divider"></div>
+        <div className="shy-sidebar-body">
+            {surface.temporaryWs && <div onMouseDown={e => surface.onChangeWorkspace(surface.temporaryWs)} key={surface.temporaryWs.id} className={'shy-sidebar-ws' + (surface.workspace.id == surface.temporaryWs.id ? " hover" : "")}>{renderWs(surface.temporaryWs)}</div>}
+            {surface.wss.map(ws => {
+                return <div onMouseDown={e => surface.onChangeWorkspace(ws)} key={ws.id} className={'shy-sidebar-ws' + (surface.workspace?.id == ws.id ? " hover" : "")}>{renderWs(ws)}</div>
+            })}
+            <a className="shy-sidebar-operator" onMouseDown={e => surface.onCreateWorkspace()} ><Icon size={24} icon={PlusSvg}></Icon></a>
+            <a className="shy-sidebar-operator" onMouseDown={e => UrlRoute.push(ShyUrl.discovery)}><Icon size={24} icon={PubWorkspace}></Icon></a>
+            {/*  <div className="shy-sidebar-divider"></div>
         <a className="shy-sidebar-operator"><Icon size={24} icon={DownloadSvg}></Icon></a> */}
+        </div>
     </div>
 })
