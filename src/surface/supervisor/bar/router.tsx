076590d066ec8b5ee2d691efe95304fee2266a38
kanhai <760489033@qq.com>
2022-06-29 00:30:17 +0800

    支持显示保存的进度条

--- src/surface/supervisor/bar/router.tsx
+++ src/surface/supervisor/bar/router.tsx
@@ -6,6 +6,7 @@ import PageSvg from "../../../assert/svg/page.svg";
 import { observer } from 'mobx-react';
 import { channel } from 'rich/net/channel';
 import { LockSvg } from 'rich/component/svgs';
+import { Loading } from 'rich/component/view/loading';
 export var PageRouter = observer(function () {
     function onClick(item) {
         if (surface.supervisor.item === item) return;
@@ -14,7 +15,8 @@ export var PageRouter = observer(function () {
     var item = surface.supervisor.item;
     if (item) {
         return <div className='shy-supervisor-bar-router'>
-            <span onMouseDown={e => onClick(item)} className='shy-supervisor-bar-router-item'><Icon icon={item.icon ? item.icon : PageSvg} size={18}></Icon><a className='shy-supervisor-bar-router-item-title'>{item.text || '新页面'}</a></span>
+            <span onMouseDown={e =>onClick(item)} className='shy-supervisor-bar-router-item'><Icon icon={item.icon ? item.icon : PageSvg} size={18}></Icon><a className='shy-supervisor-bar-router-item-title'>{item.text || '新页面'}</a></span>
+            {item.snapSaving && <span  className='shy-supervisor-bar-router-save'><Loading></Loading>保存中...</span>}
             {item.locker?.userid && <div className='shy-supervisor-bar-router-locker'><Icon size={18} icon={LockSvg}></Icon></div>}
         </div>
     }
