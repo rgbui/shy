67495a0ae304997b0d8c9d748c448ca9ed896275
kanhai <760489033@qq.com>
2022-07-31 10:06:51 +0800

    增加及优整语言国际化及外观的功能

--- src/surface/user/settings/app/appear.tsx
+++ src/surface/user/settings/app/appear.tsx
@@ -6,17 +6,8 @@ import { Remark } from "rich/component/view/text";
 export class ShyAppear extends React.Component {
     render() {
         return <div className="shy-app-lang">
-            <h2 className="h2">语言</h2>
+            <h2 className="h2">外观</h2>
             <Divider></Divider>
-            <Row style={{ marginTop: 20 }}>
-                <Col span={12}><Remark>选择语言</Remark></Col>
-                <Col span={12} align={'end'}><Select
-                    dropStyle={{ width: 80 }}
-                    dropAlign="right"
-                    value={'zh'}
-                    options={[{ text: '中文', value: 'zh' }]}
-                ></Select></Col>
-            </Row>
         </div>
     }
 }
