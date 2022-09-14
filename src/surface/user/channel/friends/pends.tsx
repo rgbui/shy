192d50dfdfee4ca244cb8fee0d74824b868ada75
kanhai <760489033@qq.com>
2022-09-14 23:49:18 +0800

    优化用户待加用户列表

--- src/surface/user/channel/friends/pends.tsx
+++ src/surface/user/channel/friends/pends.tsx
@@ -3,10 +3,11 @@ import { Avatar } from "rich/component/view/avator/face";
 import { channel } from "rich/net/channel";
 import React from "react";
 import { surface } from "../../..";
-import { IconButton } from "rich/component/view/icon";
-import { CheckSvg, CloseTickSvg } from "rich/component/svgs";
+import { Icon } from "rich/component/view/icon";
+import { CheckSvg, CloseSvg } from "rich/component/svgs";
 import { Input } from "rich/component/view/input";
 import { userChannelStore } from "../store";
+import { ToolTip } from "rich/component/view/tooltip";
 
 export var PendListView = observer(function () {
     var refInput = React.useRef<Input>(null);
@@ -28,22 +29,25 @@ export var PendListView = observer(function () {
         userChannelStore.loadPends();
     }, [])
     return <div className="shy-friends">
-        <div className="shy-friends-search"><Input ref={e => refInput.current = e} placeholder="搜索"  clear /></div>
-        <div className="shy-friends-head"><span>待处理数-{userChannelStore.pends.total}</span></div>
+        {/* <div className="shy-friends-search"><Input ref={e => refInput.current = e} placeholder="搜索" clear /></div> */}
+        <div className="shy-friends-head"><span className="f-12">待处理数-{userChannelStore.pends.total}</span></div>
         <div className="shy-friends-list">
             {userChannelStore.pends.list.map(r => {
                 return <div key={r.id} className='shy-friends-user'>
-                    {r.userid == surface.user?.id && <><Avatar showName size={32} userid={r.friendId}></Avatar>
-                        <span>已发送好友请求</span>
-                        <div className="shy-friends-user-operator">
-                            <IconButton width={24} size={14} wrapper icon={CloseTickSvg} onMouseDown={e => removeSend(r)}></IconButton>
+                    {r.friendId != surface.user?.id && r.userid == surface.user?.id && <>
+                        <div className="flex-fixed w-200 flex"><Avatar showName size={32} userid={r.friendId}></Avatar></div>
+                        <span className="flex-auto text-1 f-12">已发送好友请求</span>
+                        <div className="flex-fixed flex-end" style={{ paddingRight: 120 }}>
+                            <ToolTip overlay={"撤消好友请求"}><span className="size-24 flex-center round item-hover cursor" onMouseDown={e => removeSend(r)}><Icon size={16} icon={CloseSvg}></Icon></span></ToolTip>
                         </div>
                     </>
                     }
-                    {r.friendId == surface.user?.id && <><Avatar size={32} userid={r.friendId}></Avatar>
-                        <span>来自ta的好友请求</span>
-                        <div className="shy-friends-user-operator">
-                            <IconButton width={24} size={14} wrapper icon={CheckSvg} onMouseDown={e => agree(r)}></IconButton>
+                    {r.friendId == surface.user?.id && <>
+                        <div className="flex-fixed w-200 flex"><Avatar showName size={32} userid={r.friendId}></Avatar></div>
+                        <span className="flex-auto text-1  f-12">来自ta的好友请求</span>
+                        <div className="flex-fixed flex-end" style={{ paddingRight: 120 }}>
+                            <ToolTip overlay={"同意好友请求"}><span className="size-24 flex-center round item-hover cursor gap-r-10" onMouseDown={e => agree(r)}><Icon size={16} icon={CheckSvg}></Icon></span></ToolTip>
+                            <ToolTip overlay={"拒绝好友请求"}><span className="size-24 flex-center round item-hover cursor" onMouseDown={e => removeSend(r)}><Icon size={16} icon={CloseSvg}></Icon></span></ToolTip>
                         </div>
                     </>}
                 </div>
