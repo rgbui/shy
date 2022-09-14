ce6944da1503d953303c9708fa67398efef10068
kanhai <760489033@qq.com>
2022-09-14 23:48:20 +0800

    调整页面头部的样式

--- src/surface/supervisor/bar/index.tsx
+++ src/surface/supervisor/bar/index.tsx
@@ -20,23 +20,23 @@ export var Bar = observer(function (props: { store: PageViewStore }) {
     }
     var item = props.store.item;
     if (!surface.showSlideBar) {
-        return <div className='shy-supervisor-bar'>
-            <div className='shy-supervisor-bar-left'>
+        return <div className='shy-supervisor-bar flex'>
+            <div className='shy-supervisor-bar-left flex-auto'>
                 {!surface.showSln && <a href='https://shy.live' className='shy-supervisor-bar-logo'><img src={HomeSrc} /></a>}
                 <PageRouter store={props.store}></PageRouter>
             </div>
-            <div className='shy-supervisor-bar-right'>
+            <div className='shy-supervisor-bar-right w-200  flex-fixed h-40 flex-end'>
                 <PageUsers store={props.store}></PageUsers>
                 {surface.user.isSign && <a className='shy-supervisor-bar-button' onMouseDown={e => back()}>返回我的空间</a>}
                 {!surface.user.isSign && <a className='shy-supervisor-bar-button' href='https://shy.live/sign/in'>登录</a>}
             </div>
         </div>
     }
-    return <div className='shy-supervisor-bar'>
-        <div className='shy-supervisor-bar-left'>
+    return <div className='shy-supervisor-bar  flex'>
+        <div className='shy-supervisor-bar-left flex-auto flex'>
             <PageRouter store={props.store}></PageRouter>
         </div>
-        <div className='shy-supervisor-bar-right'>
+        <div className='shy-supervisor-bar-right w-200  flex-fixed h-40  flex-end'>
             <PageUsers store={props.store}></PageUsers>
             {item && [PageLayoutType.doc, PageLayoutType.board].includes(item.pageType) && <Tooltip placement='bottom' overlay={'搜索'}><a onClick={e => props.store.onSearch(e)}><Icon size={14} icon={SearchSvg}></Icon></a></Tooltip>}
             {item && [PageLayoutType.textChannel].includes(item.pageType) && <Tooltip placement='bottom' overlay={'成员'} ><a onClick={e => props.store.onMembers(e)}><Icon size={20} icon={MemberSvg}></Icon></a></Tooltip>}
