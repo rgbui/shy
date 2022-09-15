efc8af0a94440760c4f057073704ee9871e3bb98
kanhai <760489033@qq.com>
2022-09-16 00:05:08 +0800

    增加用户私信的类型申明

---
+++ src/surface/user/channel/declare.ts
@@ -0,0 +1,38 @@
+import { IconArguments } from "rich/extensions/icon/declare";
+
+export interface UserChannel {
+    id: string;
+    createDate: Date;
+    userid: string;
+    roomId: string;
+    lastDate: Date;
+    isDeleted: boolean;
+    deletedDate: Date;
+    deletedUser: string
+}
+
+export interface UserRoom {
+    id: string;
+    creater: string;
+    createDate: Date;
+    users: { userid: string, createDate: Date, role?: 'member' | 'admin' }[];
+    name: string;
+}
+
+export interface UserCommunicate {
+    id: string;
+    createDate: Date;
+    userid: string;
+    roomId: string;
+    seq: number;
+    file: IconArguments;
+    content: string;
+    isDeleted: boolean;
+    deletedDate: Date;
+    deletedUser: string;
+    editDate: Date;
+    isEdited: boolean;
+    replyId: string;
+    reply?: UserCommunicate;
+    emojis: { emojiId: string, code?: string, count: number }[];
+}
