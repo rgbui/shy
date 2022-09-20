26d72b0f7b1c00cd75d02f5cce017e6bd2f968bf
kanhai <760489033@qq.com>
2022-09-20 23:50:30 +0800

    增加聊天接口的调用

--- src/surface/user/channel/communicate/render.tsx
+++ src/surface/user/channel/communicate/render.tsx
@@ -1,27 +1,30 @@
+import lodash, { chain } from "lodash";
 import { observer } from "mobx-react";
 import React from "react";
 import { ViewChats } from "rich/extensions/chats";
 import { ChannelTextType } from "rich/extensions/chats/declare";
 import { EmojiCode } from "rich/extensions/emoji/store";
+import { channel } from "rich/net/channel";
 import { surface } from "../../..";
 import { userChannelStore } from "../store";
 
-export var RenderChatsView = observer(function () {
+export var RenderChatsView = observer(function (props:{replyChat(d: ChannelTextType):any})
+    {
     async function delChat(d: ChannelTextType) {
-
+        await channel.del('/user/chat/cancel', { roomId: userChannelStore.currentChannel.roomId, id: d.id })
+        lodash.remove(userChannelStore.currentChannel.room.chats, x => x.id == d.id)
     }
     async function emojiChat(d: ChannelTextType, re: Partial<EmojiCode>) {
 
     }
     async function patchChat(d: ChannelTextType, data: { content: string }) {
-
+        await channel.patch('/user/chat/patch', { roomId: userChannelStore.currentChannel.roomId, content:data.content, id: d.id })
+        Object.assign(d, data);
     }
     async function reportChat(d: ChannelTextType) {
 
     }
-    async function replyChat(d: ChannelTextType) {
-
-    }
+   
     return <div className="shy-user-channel-chats">
         <ViewChats
             chats={userChannelStore.currentChannel.room.chats}
@@ -30,7 +33,7 @@ export var RenderChatsView = observer(function () {
             emojiChat={emojiChat}
             patchChat={patchChat}
             reportChat={reportChat}
-            replyChat={replyChat}
+            replyChat={props.replyChat}
         ></ViewChats>
     </div>
 })
