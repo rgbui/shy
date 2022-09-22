695167396c4c4ce4a07963d0eea403d94026849c
kanhai <760489033@qq.com>
2022-09-22 22:43:24 +0800

    实现对channel的操作

--- src/surface/user/channel/communicate/render.tsx
+++ src/surface/user/channel/communicate/render.tsx
@@ -1,47 +1,56 @@
-import lodash from "lodash";
-import { observer, useLocalObservable } from "mobx-react";
 import React from "react";
 import { ViewChats } from "rich/extensions/chats";
 import { ChannelTextType } from "rich/extensions/chats/declare";
 import { EmojiCode } from "rich/extensions/emoji/store";
 import { channel } from "rich/net/channel";
 import { surface } from "../../..";
-import { userChannelStore } from "../store";
-
-export var RenderChatsView = observer(function (props: { replyChat(d: ChannelTextType): any }) {
-    var local = useLocalObservable<{ viewChats: ViewChats }>(() => {
-        return {
-            viewChats: null
-        }
-    })
-    async function delChat(d: ChannelTextType) {
-        await channel.del('/user/chat/cancel', { roomId: userChannelStore.currentChannel.roomId, id: d.id })
-        lodash.remove(userChannelStore.currentChannel.room.chats, x => x.id == d.id)
+import { UserChannel } from "../declare";
+export class RenderChatsView extends React.Component<{
+    userChannel: UserChannel,
+    reditChat(d: ChannelTextType): any,
+    replyChat(d: ChannelTextType): any
+}>{
+    delChat = async (d: ChannelTextType) => {
+        return await channel.del('/user/chat/cancel', { roomId: this.props.userChannel.roomId, id: d.id })
     }
-    async function emojiChat(d: ChannelTextType, re: Partial<EmojiCode>) {
+    emojiChat = async (d: ChannelTextType, re: Partial<EmojiCode>) => {
+        var result = await channel.put('/user/chat/emoji', {
+            id: d.id,
+            roomId: this.props.userChannel.roomId,
+            emoji: {
+                emojiId: re.code,
+                code: re.code
+            }
+        })
+        return result;
+    }
+    patchChat = async (d: ChannelTextType, data: { content: string }) => {
+        return await channel.patch('/user/chat/patch', {
+            roomId: this.props.userChannel.roomId,
+            content: data.content, id: d.id
+        })
+    }
+    reportChat = async (d: ChannelTextType) => {
 
     }
-    async function patchChat(d: ChannelTextType, data: { content: string }) {
-        await channel.patch('/user/chat/patch', { roomId: userChannelStore.currentChannel.roomId, content: data.content, id: d.id })
-        Object.assign(d, data);
+    reditChat = async (d: ChannelTextType) => {
+        this.props.reditChat(d);
     }
-    async function reportChat(d: ChannelTextType) {
-
+    render() {
+        var props = this.props;
+        return <div className="shy-user-channel-chats">
+            <ViewChats ref={e => props.userChannel.room.viewChats = e}
+                chats={props.userChannel.room.chats}
+                user={surface.user}
+                redit={this.reditChat}
+                delChat={this.delChat}
+                emojiChat={this.emojiChat}
+                patchChat={this.patchChat}
+                reportChat={this.reportChat}
+                replyChat={props.replyChat}
+            ></ViewChats>
+        </div>
     }
+}
 
-    React.useEffect(() => {
-        console.log('ggg');
-        if (local.viewChats) local.viewChats.updateChats(userChannelStore.currentChannel.room.chats)
-    }, [userChannelStore.currentChannel.room.chats])
-    return <div className="shy-user-channel-chats">
-        <ViewChats ref={e => local.viewChats = e}
-            chats={userChannelStore.currentChannel.room.chats}
-            user={surface.user}
-            delChat={delChat}
-            emojiChat={emojiChat}
-            patchChat={patchChat}
-            reportChat={reportChat}
-            replyChat={props.replyChat}
-        ></ViewChats>
-    </div>
-})
+
