d9798c010b027e6b9a2c53a9ec51cebea479d29e
kanhai <760489033@qq.com>
2022-09-25 23:30:05 +0800

    增加打开页面时，加载当前页面的在线用户

--- src/surface/supervisor/bar/user.tsx
+++ src/surface/supervisor/bar/user.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 import { Avatar } from 'rich/component/view/avator/face';
 import { surface } from '../..';
 import { PageViewStore } from '../view/store';
-
 export var PageUsers = observer(function (props: { store: PageViewStore }) {
+    React.useEffect(() => {
+        surface.workspace.loadViewOnlineUsers(item.id);
+    },[])
     var item = props.store.item;
     if (item) {
-        var users = surface.workspace.onlineUsers.get(item.id) || [];
-        var size = 28;
-        return <div className='shy-supervisor-bar-users' style={{ height: size }}>
-            {users.findAll(user => user ? true : false).map(user => {
-                return <Avatar key={user.userid} size={size} userid={user.userid}></Avatar>
-            })}
-        </div>
+        var viewUsers = surface.workspace.viewOnlineUsers.get(item.id);
+        if (viewUsers) {
+            return <div className='shy-supervisor-bar-users' style={{ height: 28 }}>
+                {viewUsers.users.map(user => {
+                    return <Avatar key={user} size={28} userid={user}></Avatar>
+                })}
+            </div>
+        }
     }
-    else return <></>;
+    return <></>;
 })
