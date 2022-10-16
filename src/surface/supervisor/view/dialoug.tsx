a8e5f0381901af6ece69dd1417afc73e914c6b82
kanhai <760489033@qq.com>
2022-10-16 22:34:36 +0800

    实现表单弹窗

--- src/surface/supervisor/view/dialoug.tsx
+++ src/surface/supervisor/view/dialoug.tsx
@@ -1,36 +1,45 @@
 import { makeObservable, observable } from "mobx";
+import { observer } from "mobx-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { popoverLayer } from "rich/component/lib/zindex";
 import { Bar } from "../bar";
 import { createPageContent } from "./page";
 import { PageViewStore } from "./store";
-export class PageSupervisorDialog extends React.Component<{ store: PageViewStore }> {
+
+@observer
+export class PageSupervisorDialog extends React.Component<{ store: PageViewStore }>{
     node: HTMLElement;
     constructor(props) {
         super(props);
-        makeObservable(this, { loading: observable });
+        makeObservable(this, {
+            loading: observable
+        });
         if (!this.node) this.node = document.body.appendChild(document.createElement('div'));
     }
     loading: boolean = false;
     componentDidMount(): void {
-        // this.props.store.view = this;
+        this.load()
+    }
+    async load() {
+        this.props.store.view = this;
         this.loading = true;
-        createPageContent(this.props.store);
+        await createPageContent(this.props.store);
         this.loading = false;
     }
     componentWillUnmount(): void {
         popoverLayer.clear(g => g.obj === this);
         if (this.props.store.page)
-            this.props.store.page.cacheFragment();
+            this.props.store.page.cacheFragment()
         if (this.node) {
-            ReactDOM.unmountComponentAtNode(this.node);
+            // ReactDOM.unmountComponentAtNode(this.node);
             this.node.remove()
+            this.node = null;
         }
     }
     mousedown(event: React.MouseEvent) {
         var ele = event.target as HTMLElement;
-        if (ele.className == 'shy-supervisor-dialog-mask') {
+        if (ele.classList.contains('mask')) {
             this.onClose();
         }
     }
@@ -39,10 +48,14 @@ export class PageSupervisorDialog extends React.Component<{ store: PageViewStore
     }
     pageEl: HTMLElement;
     render() {
-        return ReactDOM.createPortal(<div onMouseDown={e => this.mousedown(e)} style={{ zIndex: popoverLayer.zoom(this) }} className="shy-supervisor-dialog-mask"><div className="shy-supervisor-dialog">
-            <Bar store={this.props.store}></Bar>
-            <div className="shy-supervisor-view-content" ref={e => this.pageEl = e}>
+        return ReactDOM.createPortal(<div
+            onMouseDown={e => this.mousedown(e)}
+            style={{ zIndex: popoverLayer.zoom(this) }}
+            className="fixed-full mask flex-center"><div style={{ marginTop: 72, marginBottom: 72, height: 'calc(100% - 144px)' }} className="round shadow bg-white min-w-960">
+                <Bar className={'round'} noPosition store={this.props.store}></Bar>
+                <div className="shy-supervisor-view-content overflow-y" style={{ height: 'calc(100% - 48px)' }} ref={e => this.pageEl = e}>
+                </div>
             </div>
-        </div></div>, this.node)
+        </div>, this.node)
     }
 }
