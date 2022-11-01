c4e04702743e5db17dfac3a06edc9f3f2d7c0936
kanhai <760489033@qq.com>
2022-11-01 09:03:43 +0800

    更换成网络请求加载markdown

--- src/surface/user/settings/app/upgrade.tsx
+++ src/surface/user/settings/app/upgrade.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { Divider } from "rich/component/view/grid";
+import { Markdown } from "rich/component/view/markdown";
+import { Spin } from "rich/component/view/spin";
 import { Remark } from "rich/component/view/text";
-import md from "../../../../../CHANGELOG.zh-CN.md";
+import { util } from "rich/util/util";
 export class ShyAppUpdate extends React.Component {
+    md: string = '';
+    loading = true;
+    componentDidMount() {
+        this.load()
+    }
+    async load() {
+        var d = await import('../../../../../CHANGELOG.zh-CN.md');
+        var url = d.default;
+        this.md = await util.getText(url);
+        this.loading = false;
+        this.forceUpdate()
+    }
     render() {
         return <div className="shy-app-update">
             <h2 className="h2">更新日志</h2>
             <Divider></Divider>
             <Remark>记录着我们成长的脚步，也记录着大家对我们的期望</Remark>
-            <div className="shy-app-update-content" dangerouslySetInnerHTML={{ __html: md }}></div>
+            <div className="shy-app-update-content">
+                {this.loading && <Spin block></Spin>}
+                <Markdown md={this.md}></Markdown>
+            </div>
         </div>
     }
 }
