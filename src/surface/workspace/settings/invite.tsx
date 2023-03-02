694c409fe412f095482bb7aafd76b60fb06924ff
kanhai <760489033@qq.com>
2023-03-03 02:26:55 +0800

    fix:修复邀请的地址不正常的问题

--- src/surface/workspace/settings/invite.tsx
+++ src/surface/workspace/settings/invite.tsx
@@ -4,6 +4,7 @@ import { Button } from "rich/component/view/button";
 import { Divider } from "rich/component/view/grid";
 import { Input } from "rich/component/view/input";
 import { surface } from "../..";
+import { config } from "../../../common/config";
 
 @observer
 export class WorkspaceInvite extends React.Component {
@@ -13,18 +14,24 @@ export class WorkspaceInvite extends React.Component {
     }
     input: Input;
     render() {
+        var url = `https://${surface.workspace.siteDomain || surface.workspace.sn}.shy.live/`
+        if (surface.workspace.customSiteDomain) {
+            url = `${surface.workspace.customSiteDomainProtocol || 'https'}://${surface.workspace.customSiteDomain}/`
+        }
+        if (config.isDev || config.isBeta) {
+            url = location.protocol + '//' + location.host + "/"
+        }
         return <div className='shy-ws-invites'>
             <div className="h2">邀请</div>
             <Divider></Divider>
             <div className="bold f-14 gap-t-10">通用的邀请链接</div>
             <div className="remark f-12 gap-h-10">
-                任何得到此链接的人均可以加入这个空间(空间没有人数上限的限制)，你也可以<a className="link undeline" onMouseDown={e => this.createInvite(true)}>重置链接</a>
+                任何得到此链接的人均可以加入这个空间(空间没有人数上限的限制)，你也可以<a className="link undeline cursor" onMouseDown={e => this.createInvite(true)}>重置链接</a>
             </div>
             <div className="flex max-w-500">
                 <Input ref={e => this.input = e}
                     readonly={true}
-                    value={surface.workspace.invite ? location.protocol + '//' + location.host + '/invite/' + surface.workspace.invite : ''}></Input>
-
+                    value={surface.workspace.invite ? url + '/invite/' + surface.workspace.invite : ''}></Input>
                 <Button className="gap-l-10"
                     onClick={e => this.createInvite()}
                     ghost>{surface.workspace.invite ? '复制链接' : '创建链接'}</Button>
