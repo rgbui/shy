cadd2456f347774512804c35f8ff82607b629337
kanhai <760489033@qq.com>
2023-03-10 22:55:49 +0800

    增加签名或验签的库

--- src/common/sign.ts
+++ src/common/sign.ts
@@ -4,51 +4,74 @@
  * 
  * 签名
  */
+
+import { ethers } from "ethers";
+
 import { util } from "rich/util/util.js";
 import { CacheKey, sCache } from "../../net/cache/index.js";
 import "../assert/js/jsrsasign-all-min.js"
 import { surface } from "../surface";
-
+import { stringify } from "json-stable-stringify"
 
 
 /**
  * 签名的方式版本
  * 
  */
-const SIGN_VERSION = 'sign-v1';
+const SIGN_VERSION = 'shy-sign-v1';
 
-export async function signContent(data: Record<string, any>, contentType: string) {
 
-    var content: string = '';
-    var md5: string = '';
+function getRowContentData(row: Record<string, any>, contentType: string) {
+
+    return row;
+}
+export async function signContent(row: Record<string, any>, contentType: string) {
+    row = getRowContentData(row, contentType);
     var random = util.getRandom(1000, 1e8);
     var timestamp = Date.now();
     var device = await sCache.get(CacheKey.device);
 
-    var sig = new (window as any).jsrsasign.Signature({ alg: 'SHA1withRSA' });
-    sig.init(surface.user.rk);
-    sig.updateString(md5);
-    var sigVal = sig.sign();
-    return {
-        val: sigVal,
+    var domain = {
         ver: SIGN_VERSION,
         userid: surface.user.id,
-        uk: surface.user.uk,
-        type: contentType,
+        address: surface.user.uk,
+        blockId: surface.workspace.chainBlockId,
         timestamp,
         random,
-        device
-    };
+        device,
+        type: contentType,
+    }
+
+    var message: string = stringify({ domain, row })
+    if (typeof message != 'string') message = JSON.stringify(message)
+    message = ethers.utils.hashMessage(message);
+    var wallet = new ethers.Wallet(surface.user.rk)
+    var sign = await wallet.signMessage(message);
+    return util.base64En(JSON.stringify({
+        sign: sign,
+        message: message,
+        domain
+    }));
 }
 
-export async function verifySign(data: Record<string, any>, contentType: string) {
-    var sign = data.sign;
-    if (typeof sign == 'string') sign = JSON.parse(sign);
-    var md5 = sign.md5;
-    var sig2 = new (window as any).jsrsasign.Signature({ "alg": "SHA1withRSA", prvkeypem: sign.uk });
-    //sig2.init(PUBLIC);
-    sig2.updateString(md5);
-    var isValid = sig2.verify(sign.val);
-
-    return isValid
+export async function verifyRow(row: Record<string, any>) {
+    var signMessage = row.sign;
+    if (signMessage) {
+        var sm = JSON.parse(util.base64De(signMessage))
+        var address = ethers.utils.verifyMessage(sm.message, sm.sign)
+        if (address == sm.domain.address) {
+            //说明签名没问题，下面验证内容是否被窜改了
+            row = getRowContentData(row, sm.domain.type);
+            var message: string = stringify({ domain: sm.domain, row })
+            if (typeof message != 'string') message = JSON.stringify(message)
+            var message = ethers.utils.hashMessage(message);
+            //说明数据的哈希值是一样的
+            if (message == sm.message) {
+                return true;
+            }
+        }
+        return false;
+    }
+    else return null;
 }
+
