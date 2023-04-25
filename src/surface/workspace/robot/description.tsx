9c46ad8e7fc7d6f556c6eda6178f029430d57075
kanhai <760489033@qq.com>
2023-04-25 10:54:54 +0800

    feature: add robot description

---
+++ src/surface/workspace/robot/description.tsx
@@ -0,0 +1,85 @@
+import { makeObservable, observable } from "mobx";
+import { observer } from "mobx-react";
+import React from "react";
+import { ShyAlert } from "rich/component/lib/alert";
+import { EditSvg } from "rich/component/svgs";
+import { Button } from "rich/component/view/button";
+import { Icon } from "rich/component/view/icon";
+import { Textarea } from "rich/component/view/input/textarea";
+import { Markdown } from "rich/component/view/markdown";
+import { RobotInfo } from "rich/types/user";
+import { masterSock } from "../../../../net/sock";
+
+@observer
+export class RobotInfoDescriptionView extends React.Component<{ robot: RobotInfo }>{
+    constructor(props) {
+        super(props);
+        if (!this.props.robot.remark) this.isEdit = true;
+        this.remark = this.props.robot.remark;
+        makeObservable(this, {
+            isEdit: observable,
+            remark: observable
+        });
+    }
+    isEdit: boolean = false;
+    remark: string = '';
+    onSave = async (b: Button) => {
+        if (!this.remark) return ShyAlert('请介绍一下机器人,如何使用它')
+        try {
+            b.loading = true;
+            var rs = this.remark.split(/\r?\n/g);
+            for (let i = 0; i < rs.length; i++) {
+                if (!rs[i].endsWith('  ')) rs[i] += "  "
+            }
+            this.remark = rs.join("\n");
+            await masterSock.patch('/robot/set', {
+                id: this.props.robot.id,
+                data: {
+                    remark: this.remark
+                }
+            })
+            b.loading = false;
+            this.props.robot.remark = this.remark;
+            this.isEdit = false;
+        }
+        catch (ex) {
+            console.error(ex)
+            b.loading = false;
+        }
+        finally {
+
+        }
+    }
+    render() {
+        var robot = this.props.robot;
+        return <div>
+            {this.isEdit && <div>
+                <div className="flex gap-h-10">
+                    <div className="flex-auto remark">介绍一下机器人</div>
+                    <div className="flex-fixed flex  r-gap-l-10">
+                        <Button onClick={(e, b) => this.onSave(b)} >保存</Button>
+                        <Button onClick={e => this.isEdit = false} ghost>退出编辑</Button>
+                    </div>
+                </div>
+                <Textarea
+                    style={{ height: 500 }}
+                    placeholder="介绍一下机器人,如何使用它，支持markdown语法"
+                    value={robot.remark}
+                    onChange={e => this.remark = e}
+                ></Textarea>
+            </div>}
+            {!this.isEdit && <div className="visible-hover">
+                <div className="visible flex h-30">
+                    <span className="flex-auto"></span>
+                    <span className="flex-fixed link underline cursor flex r-gap-l-5 f-12"
+                        onMouseDown={e => this.isEdit = true}
+                    >
+                        <Icon size={14} icon={EditSvg}></Icon>
+                        <span>编辑</span>
+                    </span>
+                </div>
+                <Markdown md={this.remark}></Markdown>
+            </div>}
+        </div>
+    }
+}
