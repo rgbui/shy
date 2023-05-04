c903eb60ee5ec94d9de2dcead84102423068a456
kanhai <760489033@qq.com>
2023-05-04 18:53:06 +0800

    fix:优化workpace memeber的成员管理

--- src/surface/workspace/settings/member/member.tsx
+++ src/surface/workspace/settings/member/member.tsx
@@ -8,47 +8,71 @@ import { surface } from '../../../store';
 import { Select } from 'rich/component/view/select';
 import { channel } from 'rich/net/channel';
 import { Avatar } from 'rich/component/view/avator/face';
-import { PlusSvg } from 'rich/component/svgs';
+import { CloseSvg, PlusSvg } from 'rich/component/svgs';
 import { Icon } from 'rich/component/view/icon';
 import lodash from 'lodash';
 import { useSelectMenuItem } from 'rich/component/view/menu';
 import { Rect } from 'rich/src/common/vector/point';
 import { ShyAlert } from 'rich/component/lib/alert';
-
+import { SearchListType } from 'rich/component/types';
+import { Spin } from 'rich/component/view/spin';
+import { Pagination } from 'rich/component/view/pagination';
+import { Confirm } from 'rich/component/lib/confirm';
+import { ToolTip } from 'rich/component/view/tooltip';
 
 @observer
 export class WorkspaceMembers extends React.Component {
     constructor(props) {
         super(props);
         makeObservable(this, {
-            memebers: observable
+            searchList: observable,
+            currentRobots: observable
         })
     }
     componentDidMount() {
-        this.loadMembers();
+        (async () => {
+            await this.loadCurrentRobots();
+            await this.loadMembers();
+        })()
     }
     async loadMembers() {
-        var r = await channel.get('/ws/members', {
-            page: this.page,
-            size: this.size,
-            word: this.word || undefined,
-            roleId: this.roleId || undefined
-        });
-        if (r.ok) {
-            runInAction(() => {
-                this.page = r.data.page;
-                this.size = r.data.size;
-                this.total = r.data.total;
-                this.memebers = r.data.list;
-            })
+        this.searchList.loading = true;
+        try {
+            var r = await channel.get('/ws/members', {
+                page: this.searchList.page,
+                size: this.searchList.size,
+                word: this.searchList.word || undefined,
+                roleId: this.searchList.roleId || undefined
+            });
+            if (r.ok) {
+                Object.assign(this.searchList, r.data);
+                console.log(JSON.stringify(this.searchList))
+            }
+        }
+        catch (ex) {
+            this.searchList.error = ex.toString();
+        }
+        finally {
+            this.searchList.loading = false;
         }
     }
-    roleId: string = '';
-    word: string = '';
-    memebers: any[] = [];
-    page: number = 1;
-    size: number = 100;
-    total: number = -1;
+    async loadCurrentRobots() {
+        var gs = await channel.get('/ws/robots');
+        if (gs.ok) {
+            this.currentRobots = gs.data.list as any;
+            // console.log(JSON.stringify(this.currentRobots))
+        }
+    }
+    searchList: SearchListType<any, { roleId: string }> = {
+        list: [],
+        page: 1,
+        size: 100,
+        total: 0,
+        word: '',
+        roleId: '',
+        loading: false,
+        error: ''
+    }
     async selectRole(member, event: React.MouseEvent) {
         var roles = lodash.cloneDeep(surface.workspace.roles);
         if (Array.isArray(member.roleIds)) {
@@ -57,7 +81,15 @@ export class WorkspaceMembers extends React.Component {
         if (roles.length == 0) return ShyAlert('请添加角色');
         var r = await useSelectMenuItem(
             { roundArea: Rect.fromEvent(event) },
-            roles.map(r => ({ text: r.text, value: r.id }))
+            roles.map(r => {
+                return {
+                    text: r.text,
+                    value: r.id,
+                    renderIcon(item, value) {
+                        return <span className='flex size-14 circle' style={{ background: r.color }}></span>
+                    }
+                }
+            })
         );
         if (r) {
             var g = await channel.patch('/ws/patch/member/roles', {
@@ -77,11 +109,17 @@ export class WorkspaceMembers extends React.Component {
                 userid: member.userid, roleIds: member.roleIds
             });
             if (g.ok) {
-
+                ShyAlert('移除成功')
             }
         }
     }
-    render() {
+    async removeUser(member) {
+        if (await Confirm(`确认要移除成员吗`)) {
+            channel.del('/ws/member/delete', { userid: member.userid })
+            this.loadMembers();
+        }
+    }
+    getRoleOptions() {
         var roles = surface.workspace.roles;
         var options = roles.map(r => {
             return {
@@ -90,46 +128,60 @@ export class WorkspaceMembers extends React.Component {
             }
         });
         options.splice(0, 0, { text: '@所有人', value: '' })
+        return options;
+    }
+    render() {
         return <div className='shy-ws-members'>
             <div className="h2">成员</div>
             <Divider></Divider>
             <div className='shy-ws-members-list'>
                 <div className='shy-ws-member-head'>
-                    <Row style={{ marginBottom: 0 }}>
-                        <Col span={6} style={{ height: 30 }} valign={'middle'}>
-                            <span style={{ fontSize: 14 }}>{this.total}成员</span>
-                        </Col>
-                        <Col span={18} style={{ height: 30 }} valign={'middle'} align={'end'}>
-                            <Space>
-                                <span style={{ fontSize: 14 }}>显示角色:</span>
-                                <Select style={{ fontSize: 14 }} value={this.roleId} dropStyle={{ width: 120 }} onChange={e => this.roleId = e} options={options}></Select>
-                                <Input style={{ width: 180 }} value={this.word} onChange={e => this.word = e} onEnter={e => this.loadMembers()} placeholder='搜索用户...'></Input>
-                            </Space>
-                        </Col>
-                    </Row>
+                    <div className='flex'>
+                        <div className='flex-fixed'>
+                            <span>{this.searchList.total}成员</span>
+                        </div>
+                        <div className='flex-auto flex-end'>
+                            <span style={{ fontSize: 14 }}>显示角色:</span>
+                            <Select style={{ fontSize: 14 }} value={this.searchList.roleId} dropStyle={{ width: 120 }} onChange={e => { this.searchList.roleId = e; this.loadMembers() }} options={this.getRoleOptions()}></Select>
+                            <Input style={{ width: 180 }} value={this.searchList.word} onChange={e => this.searchList.word = e} onEnter={e => this.loadMembers()} placeholder='搜索用户...'></Input>
+                        </div>
+                    </div>
                 </div>
                 <Divider></Divider>
-                {
-                    this.memebers.map(me => {
-                        return <div key={me.id} className='shy-ws-member'>
-                            <Row style={{ marginBottom: 0 }}>
-                                <Col span={8}><Avatar showName size={30} userid={me.userid}></Avatar></Col>
-                                <Col span={12}>
-                                    <div className='shy-ws-member-roles'>
-                                        {(me.roleIds || []).map(r => {
-                                            var role = roles.find(g => g.id == r);
-                                            if (role) return <a key={r}><span className='color' onMouseDown={e => this.removeRole(me, r, e)} style={{ backgroundColor: role.color }}></span><span className='text'>{role.text}</span></a>
-                                            else return <a style={{ display: 'none' }} key={r}></a>
-                                        })}
-                                        <a className='cursor'><Icon size={12} icon={PlusSvg} onClick={e => this.selectRole(me, e)}></Icon></a>
-                                    </div>
-                                </Col>
-                                <Col span={4} align={"end"}></Col>
-                            </Row>
+                {this.searchList.loading && <div className='flex-center gap-h-20'><Spin></Spin></div>}
+                {this.searchList.list.map(me => {
+                    return <div key={me.id} className='shy-ws-member flex round padding-10'>
+                        <div className='flex-fixed w-240'>
+                            <Avatar showName showSn={false} size={30} userid={me.userid}></Avatar>
                         </div>
-                    })
+                        <div className='flex-auto'>
+                            <div className='shy-ws-member-roles'>
+                                {(me.roleIds || []).map(r => {
+                                    var role = surface.workspace.roles.find(g => g.id == r);
+                                    if (role) return <a key={r}><span className='color cursor' onMouseDown={e => this.removeRole(me, r, e)} style={{ backgroundColor: role.color }}></span><span className='text'>{role.text}</span></a>
+                                    else return <a style={{ display: 'none' }} key={r}></a>
+                                })}
+                                {(me.roleIds || []).length < surface.workspace.roles.length && <a className='cursor'><Icon size={12} icon={PlusSvg} onClick={e => this.selectRole(me, e)}></Icon></a>}
+                            </div>
+                        </div>
+                        <div className='flex-fixed'>
+                            {surface.workspace.owner != me.userid && !this.currentRobots.some(s => s.userid == me.userid) && <ToolTip overlay={'移出成员'}>
+                                <span onMouseDown={e => this.removeUser(me)} className='flex-center size-24 item-hover cursor round'>
+                                    <Icon size={14} icon={CloseSvg}></Icon>
+                                </span>
+                            </ToolTip>}
+                        </div>
+                    </div>
+                })
                 }
+                <Pagination index={this.searchList.page}
+                    size={this.searchList.size}
+                    total={this.searchList.total} onChange={e => {
+                        this.searchList.page = e;
+                        this.loadMembers();
+                    }}></Pagination>
             </div>
         </div>
     }
+    currentRobots: { userid: string }[] = [];
 }
