c8d393ce8a05a4d6ed716fd1d0d29a057229b5a3
kanhai <760489033@qq.com>
2023-05-05 01:02:26 +0800

    fix: remove react-tip

--- i18n/tip.tsx
+++ i18n/tip.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppLang } from "./enum";
-import Tooltip from "rc-tooltip";
 import { SA } from './view';
+import { ToolTip } from 'rich/component/view/tooltip';
 export class AppTip extends React.Component<{
     children: React.ReactElement | React.ReactElement[],
     id?: AppLang,
@@ -17,14 +17,12 @@ export class AppTip extends React.Component<{
     private tip: any;
     render() {
         var ov = typeof this.props.id != 'undefined' ? <SA id={this.props.id}></SA> : this.props.overlay;
-        return <Tooltip overlayClassName='sy-tooltip' ref={e => this.tip = e}
+        return <ToolTip ref={e => this.tip = e}
             mouseEnterDelay={0.8}
             mouseLeaveDelay={0.1}
             placement={this.props.placement || 'top'}
-            trigger={['hover']}
-            overlayInnerStyle={{ minHeight: 'auto' }}
             overlay={<div className='sy-tooltip-content'>{ov}</div>}
         >{Array.isArray(this.props.children) ? <>{this.props.children}</> : this.props.children}
-        </Tooltip>
+        </ToolTip>
     }
 }
