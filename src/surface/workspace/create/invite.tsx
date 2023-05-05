5a9f926a723160a0eb2d9c9ee218752f4141e009
kanhai <760489033@qq.com>
2023-05-05 22:44:41 +0800

    fix:修复空间协作邀请页面的bug

--- src/surface/workspace/create/invite.tsx
+++ src/surface/workspace/create/invite.tsx
@@ -12,16 +12,20 @@ import { ShyUrl, UrlRoute } from "../../../history";
 import { WsAvatar } from "rich/component/view/avator/ws";
 import { Sock } from "../../../../net/sock";
 import { Pid } from "../declare";
-
+import { Spin } from "rich/component/view/spin";
 export var InviteView = observer(function () {
-    var local = useLocalObservable<{ ws: Partial<Workspace>, pids: Pid[], loading: boolean }>(() => {
+    var local = useLocalObservable<{
+        ws: Partial<Workspace>,
+        pids: Pid[],
+        loading: boolean
+    }>(() => {
         return {
             ws: null,
             pids: [],
             loading: true
         }
     })
-    async function trySign() {
+    async function loadInviteWorkspace() {
         var inviteCode = UrlRoute.match(ShyUrl.invite)?.id;
         if (!surface.user.isSign) {
             var url = '/invite/' + inviteCode;
@@ -30,15 +34,18 @@ export var InviteView = observer(function () {
         }
         var w = await channel.get('/ws/invite/check', { invite: inviteCode });
         if (w.ok) {
-            var rg = await channel.get('/ws/is/member', { sock: Sock.createSock(Workspace.getWsSockUrl(w.data.pids,'ws')), wsId: w.data.workspace.id })
+            var rg = await channel.get('/ws/is/member', {
+                sock: Sock.createSock(Workspace.getWsSockUrl(w.data.pids, 'ws')),
+                wsId: w.data.workspace.id
+            })
             if (rg?.data?.exists) return UrlRoute.pushToWs(w.data.workspace.siteDomain || w.data.workspace.sn);
-            local.ws = w.data.workspace;
+            local.ws = Object.assign({}, w.data.workspace || {}, rg.data.workspace || {});
             local.pids = w.data.pids;
             local.loading = false;
         }
     }
     async function join() {
-        var sock = Sock.createSock(Workspace.getWsSockUrl(local.pids,'ws'))
+        var sock = Sock.createSock(Workspace.getWsSockUrl(local.pids, 'ws'))
         await channel.put('/user/join/ws', { wsId: local.ws.id });
         var r = await channel.put('/ws/invite/join', { wsId: local.ws.id, sock });
         await surface.loadWorkspaceList();
@@ -48,13 +55,32 @@ export var InviteView = observer(function () {
         UrlRoute.push(ShyUrl.myWorkspace);
     }
     React.useEffect(() => {
-        trySign();
+        loadInviteWorkspace();
     }, [])
 
-    if (local.loading == true) return <Loading></Loading>
+    if (local.loading == true) return <div className="gap-h-30 flex-center">
+        <Spin></Spin>
+    </div>
     return <div className="shy-invite">
-        <Row align="center">邀请您加入他们的空间</Row>
-        <Row align="center" style={{ marginTop: 20, marginBottom: 30 }}><Space><Button onClick={e => join()}>加入</Button><Button onClick={e => refuse()} ghost>拒绝</Button></Space></Row>
-        <WsAvatar wsId={local.ws.id}></WsAvatar>
+        {local.ws.accessProfile?.disabledJoin && <div className="flex flex-center">
+            <div className="flex-center error gap-h-30">
+                您无法加入，空间禁止成员加入
+            </div>
+            <div>
+                <WsAvatar wsId={local.ws.id}></WsAvatar>
+            </div>
+        </div>}
+        {!local.ws.accessProfile.disabledJoin && <div>
+            <div className="flex-center">
+                邀请您加入{local.ws.text}
+            </div>
+            <div className="flex-center">
+                <Button onClick={e => join()}>加入</Button><Button onClick={e => refuse()} ghost>拒绝</Button>
+            </div>
+            <div className="flex-center">
+                <WsAvatar wsId={local.ws.id}></WsAvatar>
+            </div>
+        </div>}
+        
     </div>
 })
