6b87b775cd468323cc6a2fdfba0dddc15d945063
kanhai <760489033@qq.com>
2023-05-06 22:59:36 +0800

    fix:增加页面未访问权限判断

--- src/surface/404.tsx
+++ src/surface/404.tsx
@@ -2,6 +2,9 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import { config } from "../../common/config";
 import { ShyUrl, UrlRoute } from "../history";
+import { Divider } from "rich/component/view/grid";
+import { surface } from "./store";
+import { Button } from "rich/component/view/button";
 export var View404 = observer(function () {
     function back(event: React.MouseEvent) {
         event.preventDefault();
@@ -20,4 +23,18 @@ export var View404 = observer(function () {
             </div>
         </div>
     </div>
+})
+
+export var ViewNotAllow = observer(function () {
+    return <div className='shy-404'>
+        <div className='shy-404-content'>
+            <div className="h2 flex-center">该页面未开启公开访问</div>
+            <Divider></Divider>
+            <div>
+                <div className="flex-center remark">如您拥有该页面的访问权限，请登录后查看</div>
+                <div className="flex-center gap-h-10"><Button onClick={e => UrlRoute.push(ShyUrl.myWorkspace)}>立即使用诗云</Button></div>
+                {!(surface.user?.isSign) && <div>您尚未登录 诗云，立即<a href='https://shy.live/sign/in'>登录</a></div>}
+            </div>
+        </div>
+    </div >
 })
