d6c0b163f6387f5a041e42c295632cc9a67fbd39
kanhai <760489033@qq.com>
2023-05-07 00:01:54 +0800

    fix:优化页面的没权限的提示

--- src/surface/404.tsx
+++ src/surface/404.tsx
@@ -29,9 +29,9 @@ export var ViewNotAllow = observer(function () {
     return <div className='flex-center flex-auto v100'>
         <div className='gap-t-200 w-500'>
             <div className="h2 flex-center">该页面未开启公开访问</div>
-            <Divider></Divider>
-            <div>
-                <div className="flex-center remark">如您拥有该页面的访问权限，请登录后查看</div>
+            <Divider ></Divider>
+            <div className="gap-t-10">
+                <div className="flex-center remark">如您拥有该页面的访问权限，请{surface.user.isSign ? "刷新" : "登录"}后查看</div>
                 <div className="flex-center gap-h-20"><Button onClick={e => UrlRoute.push(ShyUrl.myWorkspace)}>立即使用诗云</Button></div>
                 {!(surface.user?.isSign) && <div className="flex-center">您尚未登录 诗云，立即<a href='https://shy.live/sign/in'>登录</a></div>}
             </div>
