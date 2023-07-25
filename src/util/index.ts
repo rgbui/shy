40d449150b57e1fb40f3e2ea2e9c6e9d5e89d951
kanhai <760489033@qq.com>
2023-07-25 08:52:39 +0800

    feature: add arrayMove

--- src/util/index.ts
+++ src/util/index.ts
@@ -65,5 +65,20 @@ export var ShyUtil = {
         const firstLetter = word.charAt(0);
         const capitalizedWord = firstLetter.toUpperCase() + word.slice(1);
         return capitalizedWord
+    },
+    arrayMove<T>(arr: T[], item: T, tindex) {
+        var index = arr.indexOf(item);
+        //如果当前元素在拖动目标位置的下方，先将当前元素从数组拿出，数组长度-1，我们直接给数组拖动目标位置的地方新增一个和当前元素值一样的元素，
+        //我们再把数组之前的那个拖动的元素删除掉，所以要len+1
+        if (index > tindex) {
+            arr.splice(tindex, 0, arr[index]);
+            arr.splice(index + 1, 1)
+        }
+        else {
+            //如果当前元素在拖动目标位置的上方，先将当前元素从数组拿出，数组长度-1，我们直接给数组拖动目标位置+1的地方新增一个和当前元素值一样的元素，
+            //这时，数组len不变，我们再把数组之前的那个拖动的元素删除掉，下标还是index
+            arr.splice(tindex + 1, 0, arr[index]);
+            arr.splice(index, 1)
+        }
     }
 }
