1898897f38ba04a562458818082d3f34cd8dec51
kanhai <760489033@qq.com>
2023-08-14 17:05:19 +0800

    fix:删除手机版本的代码，由pc版本兼容手机版本

--- src/mobile/chat/friends.tsx
+++
@@ -1,7 +0,0 @@
-import { observer } from "mobx-react"
-import React from "react";
-export var FriendsView = observer(function () {
-    return <div>
-
-    </div>
-})
--- src/mobile/chat/room.tsx
+++
@@ -1,7 +0,0 @@
-import { observer } from "mobx-react";
-import React from "react";
-export var RoomView = observer(function () {
-    return <div>
-
-    </div>
-})
