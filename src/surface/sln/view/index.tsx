43fc3ff4a11704880140d8ac29f345f103b247a5
kanhai <760489033@qq.com>
2023-10-13 00:01:08 +0800

    fix:实现发布后，侧边栏搜索

--- src/surface/sln/view/index.tsx
+++ src/surface/sln/view/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { surface } from "../../store";
 import { WorkspaceProfile } from "../../workspace/profile";
-import { observer } from "mobx-react";
+import { observer, useLocalObservable } from "mobx-react";
 import { UserProfile } from "../../user/profile";
-import { ChevronDownSvg, CubesSvg, TrashSvg } from "rich/component/svgs";
+import { ChevronDownSvg, CubesSvg, SearchSvg, TrashSvg } from "rich/component/svgs";
 import { Icon } from "rich/component/view/icon";
 import { useTemplateView } from "rich/extensions/template";
 import { config } from "../../../../common/config";
@@ -11,8 +11,18 @@ import { channel } from "rich/net/channel";
 import { AtomPermission } from "rich/src/page/permission";
 import { useTrashBox } from "rich/extensions/trash";
 import { S } from "rich/i18n/view";
+import { Input } from "rich/component/view/input";
+import { lst } from "rich/i18n/store";
+import { Mime } from "../declare";
+import { getPageIcon, getPageText } from "rich/src/page/declare";
 
 export var SlnView = observer(function () {
+    var local = useLocalObservable<{ word: string, input: Input }>(() => {
+        return {
+            word: '',
+            input: null
+        }
+    })
     React.useEffect(() => {
         function keyup(event: KeyboardEvent) {
             surface.sln.keyboardPlate.keyup(event);
@@ -94,17 +104,44 @@ export var SlnView = observer(function () {
             </div>
         }
     }
+    function renderPublishSiteSearch() {
+        return <> {surface.isPubSite && !surface.isPubSiteHideMenu && <div className="gap-10">
+            <Input ref={e => local.input = e} value={local.word} onChange={e => { local.word = e }} clear onClear={() => { local.word = '' }} prefix={<span className="flex-center size-24 remark " placeholder={lst('搜索...')}>
+                <Icon size={16} icon={SearchSvg}></Icon>
+            </span>}></Input>
+        </div>}
+            {local.word && <div>
+                {surface.workspace.findAll(g => {
+                    console.log(g.text, g.mime, g);
+                    if (g.mime == Mime.page || g.mime == Mime.table || g.mime == Mime.chatroom) {
+                        if (getPageText(g).indexOf(local.word) > -1) return true;
+                    }
+                    return false;
+                }).map(item => {
+                    return <div key={item.id} onMouseDown={e => {
+                        local.word = '';
+                        if (local.input) local.input.updateValue('');
+                        channel.air('/page/open', { item: item })
+                    }} className="flex gap-h-5 padding-h-5 padding-w-10 round item-hover">
+                        <span className="flex-fixed flex-center"><Icon size={18} icon={getPageIcon(item)}></Icon></span>
+                        <span className="flex-auto gap-l-5">{getPageText(item)}</span>
+                    </div>
+                })
+                }
+            </div>}
+        </>
+    }
     return <div className='shy-wss h100' onKeyDownCapture={e => surface.sln.keyboardPlate.keydown(e.nativeEvent)} tabIndex={1}>
         {surface.workspace && <div className={'shy-ws relative h100 flex flex-col flex-full shy-ws-' + (surface.workspace.slnStyle || 'note')}>
             {!(surface.isPubSite && surface.isPubSiteDefineBarMenu) && <WorkspaceProfile ></WorkspaceProfile>}
-            <div className='shy-ws-items' ref={e => surface.sln.el = e}>
-                {renderFavs()}
+            {renderPublishSiteSearch()}
+            {!local.word && <div className='shy-ws-items' ref={e => surface.sln.el = e}>
                 {surface.workspace.childs.map(ws => {
                     var View = surface.sln.getMimeViewComponent(ws.mime);
                     return <View key={ws.id} item={ws} deep={-1} ></View>
                 })}
                 {renderBottoms()}
-            </div>
+            </div>}
             {!surface.isPubSite && <UserProfile></UserProfile>}
         </div>}
     </div>
