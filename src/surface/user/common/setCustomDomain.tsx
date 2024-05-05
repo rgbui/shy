41564bd3381ce4bc1a9d58ec9b977a1f2b5c1fd8
kanhai <760489033@qq.com>
2024-05-05 20:03:21 +0800

    fix:优化域名的绑定逻辑，增加域名绑定的提示信息

--- src/surface/user/common/setCustomDomain.tsx
+++ src/surface/user/common/setCustomDomain.tsx
@@ -18,61 +18,69 @@ import { DateInput } from "rich/extensions/date/input";
 class CustomDomain extends EventsComponent {
     render() {
         if (!this.ws) return null;
-        return <Dialoug head={<div><S>自定义域名</S></div>} style={{ width: 400 }} className={'shy-ws-set-domain'}>
-            <div className="gap-h-10">
-                <div className="remark f-12"><label><S>域名</S></label></div>
-                <div className="gap-t-5 tap-b-10 flex">
-                    <Input className={'w-120 '} placeholder={lst('输入域名')} value={this.data.customSiteDomain} onChange={e => { this.data.customSiteDomain = e; this.forceUpdate(); }}></Input>
-                </div>
-                <div className="gap-h-20 flex">
-                    <CheckBox checked={this.data.customSiteDomainProtocol} onChange={e => { this.data.customSiteDomainProtocol = e; this.forceUpdate(); }}><S>支持HTTPS</S></CheckBox>
-                </div>
-                <div className="remark f-12"><label><S>绑定方式</S></label></div>
-                <div className="gap-t-5 tap-b-10 flex">
-                    <SelectButtons
-                        block
-                        value={this.data.customSiteDomainData.type}
-                        options={[
-                            { text: lst('自建'), value: 'self-build' },
-                            { text: lst('托管'), value: 'trust' }
-                        ]}
-                        onChange={e => {
-                            this.data.customSiteDomainData.type = e as any;
-                            this.forceUpdate()
-                        }} ></SelectButtons>
-                </div>
-                {this.data.customSiteDomainData.type == 'self-build' && <div className="gap-h-20 ">
-                    <S>暂不开放</S>
-                </div>}
-                {this.data.customSiteDomainData.type == 'trust' && <div className="gap-h-20 ">
-                    {this.data.customSiteDomainProtocol && <div>
-                        <div><S>HTTPS证书</S></div>
-                        <div className="f-12 remark"><S text="仅支持nginx_ssl证书">仅支持nginx ssl证书</S></div>
-                        <div className="f-12 remark"><S>公钥</S></div>
-                        <Textarea value={this.data.customSiteDomainData.publicKey} onChange={e => {
-                            this.data.customSiteDomainData.publicKey = e;
-                            this.forceUpdate()
-                        }}></Textarea>
-                        <div className="f-12 remark"><S>私钥</S></div>
-                        <Textarea value={this.data.customSiteDomainData.privateKey}
-                            onChange={e => {
-                                this.data.customSiteDomainData.privateKey = e;
-                                this.forceUpdate()
-                            }}
-                        ></Textarea>
-                        <div className="f-12 remark"><S>过期时间</S></div>
-                        <DateInput value={this.data.customSiteDomainData.sslDate || dayjs().add(1, "year").toDate()} onChange={e => {
-                            this.data.customSiteDomainData.sslDate = e;
-                            this.forceUpdate()
-                        }}></DateInput>
+        return <Dialoug
+            style={{ width: 500 }}
+            footer={<>
+                <div className="flex-auto">
+                    {this.error && <div className="error">
+                        {this.error}
                     </div>}
-                </div>}
-                <div>
-                    <Button block ref={e => this.button = e} onClick={e => this.save()}><S>保存</S></Button>
                 </div>
-                {this.error && <div className="error">
-                    {this.error}
-                </div>}
+                <Button className="flex-fixed" ref={e => this.button = e} onClick={e => this.save()}><S>保存</S></Button>
+            </>}
+            className={'shy-ws-set-domain'}>
+            <div className="gap-t-10">
+                <div className="remark f-12"><label><S>域名</S></label></div>
+                <div className="gap-t-5 gap-b-10 flex">
+                    <Input className={'w-120 '} placeholder={lst('域名...')} value={this.data.customSiteDomain} onChange={e => { this.data.customSiteDomain = e; this.forceUpdate(); }}></Input>
+                </div>
+                <div className="gap-t-10 flex">
+                    <CheckBox checked={this.data.customSiteDomainProtocol} onChange={e => { this.data.customSiteDomainProtocol = e; this.forceUpdate(); }}>
+                        <span className="f-12 text-1"><S>支持HTTPS</S></span>
+                    </CheckBox>
+                </div>
+                {this.data.customSiteDomainProtocol && <div>
+                    <div className="gap-t-5 gap-b-10 flex-center">
+                        <SelectButtons
+                            theme="ghost"
+                            block
+                            value={this.data.customSiteDomainData.type}
+                            options={[
+                                { text: lst('自建'), value: 'self-build' },
+                                { text: lst('托管'), value: 'trust' }
+                            ]}
+                            onChange={e => {
+                                this.data.customSiteDomainData.type = e as any;
+                                this.forceUpdate()
+                            }} ></SelectButtons>
+                    </div>
+                    {this.data.customSiteDomainData.type == 'self-build' && <div className="gap-h-20 ">
+                        <S>暂不开放...</S>
+                    </div>}
+                    {this.data.customSiteDomainData.type == 'trust' && <div className="gap-h-20 ">
+                        {this.data.customSiteDomainProtocol && <div>
+                            <div className="f-12 remark gap-t-5 "><S text="仅支持nginx_ssl证书">仅适用nginx ssl证书</S></div>
+                            <div className="f-12 remark gap-t-10 gap-b-5"><S>公钥</S></div>
+                            <Textarea value={this.data.customSiteDomainData.publicKey} onChange={e => {
+                                this.data.customSiteDomainData.publicKey = e;
+                                this.forceUpdate()
+                            }}></Textarea>
+                            <div className="f-12 remark gap-t-10 gap-b-5"><S>私钥</S></div>
+                            <Textarea value={this.data.customSiteDomainData.privateKey}
+                                onChange={e => {
+                                    this.data.customSiteDomainData.privateKey = e;
+                                    this.forceUpdate()
+                                }}
+                            ></Textarea>
+                            <div className="f-12 remark  gap-t-10 gap-b-5"><S>过期时间</S></div>
+                            <DateInput value={this.data.customSiteDomainData.sslDate || dayjs().add(1, "year").toDate()} onChange={e => {
+                                this.data.customSiteDomainData.sslDate = e;
+                                this.forceUpdate()
+                            }}></DateInput>
+                        </div>}
+                    </div>}
+                </div>
+                }
             </div>
         </Dialoug>
     }
