0f9b211b44d482f0353d55abf3ef04c5b8eecac6
kanhai <760489033@qq.com>
2024-05-22 08:38:42 +0800

    0.9.333-pro

--- org/common/footer.tsx
+++ org/common/footer.tsx
@@ -71,7 +71,7 @@ export class FooterView extends React.Component {
                 <h4><S>帮肋与支持</S></h4>
                 <a href={refShyPage('template')}><S>模板库</S></a>
                 <br />
-                <a href={refShyPage(config.isUS ? "community" : "org", config.isUS ? 3 : 4)}><S>博客</S></a>
+                <a href={refShyPage( "community", config.isUS ? 3 : 4)}><S>博客</S></a>
                 <br />
                 <a href={UrlRoute.getUrl('download')}><S>下载</S></a>
                 <br />
@@ -79,11 +79,11 @@ export class FooterView extends React.Component {
                 <br />
                 <a href={refShyPage('help')}><S>帮助中心</S></a>
                 <br />
-                <a href={refShyPage(config.isUS ? "community" : "org")}><S>云云社区</S></a>
+                <a href={refShyPage( "community" )}><S>云云社区</S></a>
                 <br />
-                <a href={refShyPage(config.isUS ? "community" : "org", config.isUS ? 3 : 4)}><S>问题反馈</S></a>
+                <a href={refShyPage( "community" , config.isUS ? 3 : 4)}><S>问题反馈</S></a>
                 <br />
-                <a href={refShyPage(config.isUS ? "community" : "org", config.isUS ? 3 : 4)}><S>更新日志</S></a>
+                <a href={refShyPage("community", config.isUS ? 3 : 4)}><S>更新日志</S></a>
                 <br />
             </div>
             {!config.isUS && <div>
