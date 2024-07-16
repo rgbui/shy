b1c3fe4b271a34e9a686b3891ad6e71510c4e1cc
kanhai <760489033@qq.com>
2024-07-17 00:08:32 +0800

    fix:增加ppt官网页面

--- org/product/ppt.tsx
+++ org/product/ppt.tsx
@@ -16,7 +16,7 @@ export function ProductPPT() {
             </div>
 
             <div className="gap-h-20">
-                <img className="w100 border round-16 shadow-s " src='../static/img/doc/pic.webp' />
+                <img className="w100 border round-16 shadow-s " alt='表达想法的新媒介' src='../static/img/ppt/ppt-1.png' />
             </div>
         </div>
 
@@ -25,19 +25,19 @@ export function ProductPPT() {
                 <div className="flex-auto gap-r-30">
                     <h1 className="f-48">设计精彩，无需编码技能</h1>
                     <p className="text-1 f-16 gap-b-20 ">轻松构建和共享动态内容</p>
-                    <ol style={{padding:0}}>
-                        <li style={{listStyle:'none'}} className="gap-h-10">
+                    <ol style={{ padding: 0 }}>
+                        <li style={{ listStyle: 'none' }} className="gap-h-10">
                             🎥  参与互动画廊、视频和嵌入内容
                         </li>
-                        <li style={{listStyle:'none'}} className="gap-h-10">
+                        <li style={{ listStyle: 'none' }} className="gap-h-10">
                             📈  使用精美的图表、图解和表格来可视化数据
                         </li>
-                        <li style={{listStyle:'none'}} className="gap-h-10">   🖼️ 使用多功能预制模板快速启动您的项目
+                        <li style={{ listStyle: 'none' }} className="gap-h-10">   🖼️ 使用多功能预制模板快速启动您的项目
                         </li>
                     </ol>
                 </div>
                 <div className="flex-fixed  w-600">
-                    <img className="w100 border round-16 shadow-s " src='../static/img/doc/pic.webp' />
+                    <img className="w100 border round-16 shadow-s " src='../static/img/ppt/ppt-2.png'  />
                 </div>
             </div>
 
@@ -46,11 +46,11 @@ export function ProductPPT() {
         <div className="shy-site-block padding-h-80">
             <div className="flex flex-top">
                 <div className="flex-fixed  w-600">
-                    <img className="w100 border round-16 shadow-s " src='../static/img/doc/pic.webp' />
+                    <img className="w100 border round-16 shadow-s " src='../static/img/ppt/ppt-3.png'  />
                 </div>
                 <div className="flex-auto gap-l-30">
                     <h1 className="f-48">设计幻灯片变得前所未有地轻松</h1>
-                    <p className="text-1 f-16 gap-b-20 ">利用 PowerPoint 中的“设计器”和“创意”，创建设计精美、影响力十足的幻灯片。</p>
+                    <p className="text-1 f-16 gap-b-20 ">键入反斜杠构建块，创建设计精美、影响力十足的幻灯片。</p>
                 </div>
             </div>
         </div>
@@ -60,19 +60,19 @@ export function ProductPPT() {
                 <div className="flex-auto gap-r-30">
                     <h1 className="f-48">易于分享和发布</h1>
                     <p className="text-1 f-16 gap-b-20 ">使用任何设备吸引用户</p>
-                    <ol  style={{padding:0}}>
-                        <li  style={{listStyle:'none'}} className="gap-h-10">
+                    <ol style={{ padding: 0 }}>
+                        <li style={{ listStyle: 'none' }} className="gap-h-10">
                             📱 在任何屏幕上提供适合移动设备的内容
                         </li>
-                        <li  style={{listStyle:'none'}} className="gap-h-10">
+                        <li style={{ listStyle: 'none' }} className="gap-h-10">
                             📊 通过内置的高级分析功能跟踪参与度
                         </li>
-                        <li  style={{listStyle:'none'}} className="gap-h-10"> 💬  实时连接和协作
+                        <li style={{ listStyle: 'none' }} className="gap-h-10"> 💬  实时连接和协作
                         </li>
                     </ol>
                 </div>
                 <div className="flex-fixed w-600">
-                    <img className="w100 border round-16 shadow-s " src='../static/img/doc/pic.webp' />
+                    <img alt="多人协作" className="w100 border round-16 shadow-s " src='../static/img/ppt/ppt-users.png' />
                 </div>
             </div>
         </div>
